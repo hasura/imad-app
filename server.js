5d13153bfca7621abbf48d7163e96d8a5ccc42c4
Shahidh K Muhammed <muhammedshahid.k@gmail.com>
2017-08-01 19:31:55 +0530

    Change port to 80

--- server.js
+++ server.js
@@ -18,7 +18,10 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
+// Do not change port, otherwise your app won't run on IMAD servers
+// Use 8080 only for local development if you already have apache running on 80
+
+var port = 80;
+app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
