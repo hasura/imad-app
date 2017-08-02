e9da7a534351a3303342885bf57c3035a19194ac
ZUNAID SORATHIYA <zunaidsorathiya9@gmail.com>
2017-08-02 18:05:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  res.sendFile(path.join(__dirname, 'ui', 'family_older_brother.png'));
 });
 
 
