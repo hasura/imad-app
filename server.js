8cbac54dce2c5566851d8fb318f59eb23a954f06
034senthil <30617633+034senthil@users.noreply.github.com>
2017-08-03 14:56:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.get('/', function (req, res) {
 });
 app.get('/:articlename', function (req, res) {
   var articlename = req.params.articlename;
-  res.send(createtemplate(articles(articlename)));
+  res.send(createtemplate(articles[articlename]));
 });
 
 
