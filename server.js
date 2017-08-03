5bdaa1b45f8062395d21ccf716d843e86015934c
034senthil <30617633+034senthil@users.noreply.github.com>
2017-08-03 11:50:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-  res.send('This is article one');
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two', function (req, res) {
   res.send('This is article two');
