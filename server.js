619097cb086e4952935c9dedc8bb61d7f3d94bd7
034senthil <30617633+034senthil@users.noreply.github.com>
2017-08-03 13:00:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,11 +5,45 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var content = {
+   title: 'Senthilkumars', 
+   heading: 'Article one',
+   content: ' Whats up bro . This is my first project and it is successful. I am really happy at this point and i want to improve this page. blah... blah... blah... blah... blah... blah... blah... blah... blah... blah... blah... blah... blah...blah... blah... blah...blah...blah...blah...blah...blah...blah...blah...vvblah...blah...blah...vblah...blah...blah...blah...vvblah...blah...blah...vblah...blah...blah...blah...blah...blah...blah...blah...blah...blah...blah...blah...blah...'
+    
+};
+function createtemplate (data) {
+    var title = data.title;
+    var heading = data.heading;
+    var content = data.content;
+var htmltemplate = 
+   ` <html>
+<head> <title>
+    ${title}
+</title>
+ <link href="/ui/style.css" rel="stylesheet" />
+</head>
+
+
+<body><div class='links'>
+    <div>
+        <a href="/">Home</a>
+    </div>
+
+<h>
+    ${heading}
+</h>
+${content}
+</div>
+</body>
+</html>
+';
+return htmltemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(create template(article-one));
 });
 app.get('/article-two', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
