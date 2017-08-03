9364bacdabaf4f1d8e2596b55f96335bdb41e40c
034senthil <30617633+034senthil@users.noreply.github.com>
2017-08-03 11:26:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/article one', function (req, res) {
+  res.send('This is article one');
+});
+app.get('/article two', function (req, res) {
+  res.send('This is article two');
+});
+app.get('/article three', function (req, res) {
+  res.send('This is article three');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
