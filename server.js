c6957c41ff3d2b64a7461f4271e7e9f583d52e64
034senthil <30617633+034senthil@users.noreply.github.com>
2017-08-03 14:54:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -53,7 +53,6 @@ app.get('/:articlename', function (req, res) {
   res.send(createtemplate(articles(articlename)));
 });
 
- 
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
