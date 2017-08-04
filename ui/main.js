41f6457d25bec5f6624ff6f481fd0902f691efdb
034senthil <30617633+034senthil@users.noreply.github.com>
2017-08-04 11:29:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var element = document.getElementById('main-text'
 );
 
 element.innerHTML = 'new value';
-var img = document.getElementById('img');
+var img = document.getElementById('madi');
 img.onclick = function()
 {
     img.style.marginleft = '100px';
