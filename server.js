79a0e46f7f5d9eae243ab039997d2c66a1e6aa28
varunwitty <30322599+varunwitty@users.noreply.github.com>
2017-08-05 22:19:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.sendFile(path.join(__dirname, 'ui', 'article-1.html'));
    });
 
 app.get('/article-two', function (req, res){
