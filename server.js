0e16b3b61132d420bd50ca5b3af7e5535b36d24f
pranshugarg <pranshugarg02@gmail.com>
2017-08-06 19:06:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/article-one', function(req,res)
 {
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.sendFile(path.join(__dirname, 'article-one.html'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
