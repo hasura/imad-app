7918ed24b17d37afeecd9824781d410cb9d97747
Vaishnavi-G <30764645+Vaishnavi-G@users.noreply.github.com>
2017-08-06 18:33:50 +0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,8 +5,19 @@ element.innerHTML='vaishnavi';
 
 //move the image
 var img= document.getElementById('madi');
+var marginLeft = 0;
 //on click is an executer function
+
+function moveRight() // when u click on the image, every 100 ms the moveRight funtion will be called.
+{
+    marginLeft= marginLeft+10;
+    img.style.marginLeft= marginLeft +'px'; 
+}
 img.onclick= function()
 {
-    img.style.marginLeft='100px';
+   // img.style.marginLeft='100px'; 
+   //when u click on the image, then it will increase the left margin to the left, so it will feel like the image is moving to the right a little.
+   
+   
+   var interval= setInterval(moveRight,100);//this means, every 100ms apply the moveright function
 }
