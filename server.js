d47864a0bbec11a4c47b4178fb4e2ea186ed11b7
Vaishnavi-G <30764645+Vaishnavi-G@users.noreply.github.com>
2017-08-06 16:18:25 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send(path.join('article one is requested and will be served here '));
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two', function (req, res) {
