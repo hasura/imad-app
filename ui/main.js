1657563511ad9c8421fdebd2384c887477400a74
Vaishnavi-G <30764645+Vaishnavi-G@users.noreply.github.com>
2017-08-06 18:07:06 +0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,12 @@
 console.log('Loaded!');
+//this is for changing the content of index.html
 var element= document.getElementById('main-text');
 element.innerHTML='vaishnavi';
+
+//move the image
+var img= document.getElementById('img');
+//on click is an executer function
+img.onclick= function()
+{
+    img.style.marginLeft='100px';
+}
