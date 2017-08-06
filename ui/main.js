0b6687be656c4e98fa3eea89c745a934d757cdb6
Vaishnavi-G <30764645+Vaishnavi-G@users.noreply.github.com>
2017-08-06 18:35:01 +0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,7 +10,7 @@ var marginLeft = 0;
 
 function moveRight() // when u click on the image, every 100 ms the moveRight funtion will be called.
 {
-    marginLeft= marginLeft+5;
+    marginLeft= marginLeft+1;
     img.style.marginLeft= marginLeft +'px'; 
 }
 img.onclick= function()
