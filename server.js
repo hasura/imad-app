5413b78c1b36e30f07f34d39dd998a3e07370c93
Vaishnavi-G <30764645+Vaishnavi-G@users.noreply.github.com>
2017-08-06 17:53:30 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/msin.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'msin.js'));
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
