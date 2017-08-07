89ed05c4d73a5261178d3357b03117ca60e76488
JyothiRaju <jyothirajub@gmail.com>
2017-08-07 15:27:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/articleOne', function (req, res) {
-  res.sendFile(path.join(__dirname,'ui','articleOne');
+  res.sendFile(path.join(__dirname,'ui','articleOne'));
 });
 app.get('/articleTwo', function (req, res) {
   res.send('Article Two requested here');
