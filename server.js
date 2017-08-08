1663538d64f2393afbfa05dcfd6d4cc5cf6ea6f8
034senthil <30617633+034senthil@users.noreply.github.com>
2017-08-08 11:05:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -48,6 +48,10 @@ return htmltemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/counter', function (req, res) {
+    counter = counter + 1;
+    res.send(counter.toString());
+});
 app.get('/:articlename', function (req, res) {
   var articlename = req.params.articlename;
   res.send(createtemplate(articles[articlename]));
