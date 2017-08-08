bc4ff6af0687e39c17a3097e1acce39f195bcda0
sandeepgv1 <sandeepgv1@gmail.com>
2017-08-07 22:01:40 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var content = {
+var articleOne = {
     title : 'Sandeep Gengineri | Article One',
     heading : 'Article One',
     date : 'Aug 06, 2017',
@@ -14,12 +14,45 @@ var content = {
         </p>`
 };
 
+function createTemplate(data){
+    var title = data.title;
+    var heading = data.heading;
+    var content =  data.content;
+    
+    var htmlTemplate = `
+        <html>
+            <head>
+            <title>
+                ${title}
+            </title>
+            <meta name="viewport" content="width=device-width,intial-scale=1"/>
+            <link href="/ui/style.css" rel="stylesheet" />
+            </head>
+            <body>
+                <div class="container">
+                    <div>
+                        <a href="/">Home</a>
+                    </div>
+                    <hr/>
+                    <h2>
+                        ${heading}
+                    </h2>
+                    <div>
+                        ${content}
+                    </div>
+                </div>
+            </body>
+        </html>
+        `;
+    return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-1', function (req, res) {
-  res.sendFile(path.join(__dirname, 'article-one.html'));
+  res.sendFile(createTemplate(articleOne));
 });
 
 app.get('/article-2', function (req, res) {
