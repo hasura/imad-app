53f4793fa64af85ed2e75f6ad577d0a6a8446a79
sandeepgv1 <sandeepgv1@gmail.com>
2017-08-07 21:49:52 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var content = {
+    title : 'Sandeep Gengineri | Article One',
+    heading : 'Article One',
+    date : 'Aug 06, 2017',
+    content :`<p>
+            This is my first Paragraph
+        </p>`
+};
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
