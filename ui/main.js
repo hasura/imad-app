ae7270ac24382a258ebd5377ad6bc20ddeb964f8
034senthil <30617633+034senthil@users.noreply.github.com>
2017-08-08 12:48:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -37,7 +37,7 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function (){
    var names =['name1', 'name2', 'name3'];
-   var list -'';
+   var list ='';
    for(var i=0; i<names.length; i++){
        
        list +- '<li>' + names[i] + '</li>';
