13919383dcdcf94beb9d1c43bf1832435f191d35
JyothiRaju <jyothirajub@gmail.com>
2017-08-08 14:39:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.s'));
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 
