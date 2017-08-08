cbe4d25fed17e253447da9b565bc0343a380eaa2
sandeepgv1 <sandeepgv1@gmail.com>
2017-08-07 22:41:55 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ function createTemplate(data){
         </html>
         `;
     return htmlTemplate;
-};
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
