e8b3dfc873a25b7abf44e923ab73dd823dde1aec
034senthil <30617633+034senthil@users.noreply.github.com>
2017-08-08 13:06:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -71,8 +71,8 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 var names = [];
-app.get('/submit-name/:name', function (req, res) {
- var name = req.params.name;
+app.get('/submit-name', function (req, res) {
+ var name = req.query.name;
  names.push(name);
  res.send(JSON.stringify(names));
 });
