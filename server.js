8417f632a9b91e9370638742fabbea2e40e4951b
sandeepgv1 <sandeepgv1@gmail.com>
2017-08-07 22:41:48 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ app.get('/', function (req, res) {
 
 app.get('/:articleName', function (req, res) {
   var articleName = req.params.articleName;
-  res.send(createTemplate(articles(articleName)));
+  res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
