a86e6df62f3789c04aa32ea5c8ebc751a336a4f4
034senthil <30617633+034senthil@users.noreply.github.com>
2017-08-08 12:09:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,10 +15,19 @@
 //};//
 
 var button= document.getElementById('counter');
-var counter = 0;
-button.onclick = function(){
+//var counter = 0;
+button.onclick = function (){
     
-    counter = counter + 1;
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readystate === XMLHttpRequest.DONE){
+        if (request.status === 200){
+           var counter = request.responseText;
     var span = document.getElementById('count');
     span.innerHTML = counter.toString();
+}
+}
+},
+request.open('GET', 'http://u034senthil.imad.hasura-app.io/counter', true);
+request.send(null);
 };
