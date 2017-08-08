f24ea9700aaa091c18dffb7e1937e83298ae484a
034senthil <30617633+034senthil@users.noreply.github.com>
2017-08-08 14:43:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -40,15 +40,7 @@ submit.onclick = function (){
     request.onreadystatechange = function () {
         if (request.readyState === XMLHttpRequest.DONE){
         if (request.status === 200){
-           var counter = request.responseText;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
-}
-}
-},
-request.open('GET', 'http://u034senthil.imad.hasura-app.io/submit-name?name=' + name, true);
-request.send(null);
-   var names = request.responseText;
+           var names = request.responseText;
    names = JSON.parse(names);
    var list = '';
    for(var i=0; i< names.length; i++){
@@ -56,6 +48,11 @@ request.send(null);
        list += '<li>' + names[i] + '</li>';
    }
     
-    var ul =document.getElementById('name list');
+    var ul =document.getElementById('namelist');
     ul.innerHTML = list;
+}
+}
+},
+request.open('GET', 'http://u034senthil.imad.hasura-app.io/submit-name?name=' + name, true);
+request.send(null);
 };
