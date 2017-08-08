43498c598750882cf358c2eb1f1b106cd09d712a
S.Pawan Kumar <30653405+Dracarys99@users.noreply.github.com>
2017-08-08 15:44:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,11 @@ console.log('Loaded!');
 var element=document.getElementById('maintext');
 element.innerHTML='New Value';
 var img=document.getElementById('madi');
+var marginleft=0;
+function moveright(){
+    marginleft = marginleft + 10;
+    img.marginleft = marginleft + 'px';
+}
 img.onclick = function(){
-    img.style.marginleft='100px';
+    var interval = setInterval(moveright,100);
 };
