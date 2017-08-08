418f426ae4b43f2227b7c7cad39c66fa10a5a2db
034senthil <30617633+034senthil@users.noreply.github.com>
2017-08-08 11:27:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,24 @@
-console.log('Loaded!');
+//console.log('Loaded!');
 
-var element = document.getElementById('main-text'
-);
+//var element = document.getElementById('main-text'
+);//
 
-element.innerHTML = 'new value';
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight (){
-    marginLeft = marginLeft + 10;
-    img.style.marginLeft = marginLeft + 'px';
+//element.innerHTML = 'new value';
+//var img = document.getElementById('madi');
+//var marginLeft = 0;
+//function moveRight (){
+   // marginLeft = marginLeft + 10;
+   // img.style.marginLeft = marginLeft + 'px';
+//}
+//img.onclick = function (){
+   // var interval = setInterval(moveRight, 50);
+//};//
+
+var button= document.getElementById('counter');
+var counter = 0;
+button.onclick = function(){
+    
+    counter = counter + 1;
+    var span - document.getElementById('counter');
+    span.innerHTML = counter.toString();
 }
-img.onclick = function (){
-    var interval = setInterval(moveRight, 50);
-};
