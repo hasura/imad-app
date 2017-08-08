c8610e621735eb00f34854cb9b8ac41bcacb3db1
034senthil <30617633+034senthil@users.noreply.github.com>
2017-08-08 11:06:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -48,6 +48,7 @@ return htmltemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var counter = 0;
 app.get('/counter', function (req, res) {
     counter = counter + 1;
     res.send(counter.toString());
