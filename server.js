efdbd31cb78e53b90324d835efe9a4495f886cf9
S.Pawan Kumar <30653405+Dracarys99@users.noreply.github.com>
2017-08-08 15:25:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,14 +76,10 @@ app.get("/:articleName",function(req,res){
     var articleName = req.params.articleName;
     res.send(CreateTemplate(articles[articleName]));
 });
-
-/*app.get("/article-two",function(req,res){
-    res.send(CreateTemplate(articleOne));
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get("/article-three",function(req,res){
-    res.sendfile(path.join(__dirname,'ui', 'article-three.html'));
-});*/
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
