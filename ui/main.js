2eb9cc9d73ad64f1987482dfc6e62c2b337c3a51
034senthil <30617633+034senthil@users.noreply.github.com>
2017-08-08 14:47:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,8 +32,7 @@ request.open('GET', 'http://u034senthil.imad.hasura-app.io/counter', true);
 request.send(null);
 };
 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function (){
      var request = new XMLHttpRequest();
@@ -53,6 +52,8 @@ submit.onclick = function (){
 }
 }
 },
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
 request.open('GET', 'http://u034senthil.imad.hasura-app.io/submit-name?name=' + name, true);
 request.send(null);
 };
