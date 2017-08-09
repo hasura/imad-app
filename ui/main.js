00a0da75b82073917ee0cf9f0b470482ae67d31e
JyothiRaju <jyothirajub@gmail.com>
2017-08-09 12:24:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,7 @@ console.log("Loaded");
 var element=document.getElementById('main-text');
 element.innerHTML='New value';
 var img=document.getElementById('madi');
+var marginLeft=0;
 function moveRight(){
     marginLeft=marginLeft+10;
     img.style.marginLeft=marginLeft+'px';
