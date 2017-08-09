bde6647efbbee1b7545f125e89c8d1176afadcdd
Amusseus <30673952+Amusseus@users.noreply.github.com>
2017-08-08 18:07:26 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send("Article one will be uploaded in a few days");
+ res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two', function (req, res) {
