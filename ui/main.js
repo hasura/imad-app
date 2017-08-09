312b8ba3a2df03534db9aa62f8eb5d4cb1c642ca
JyothiRaju <jyothirajub@gmail.com>
2017-08-09 13:59:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,8 @@
-console.log("Loaded");
-var element=document.getElementById('main-text');
-element.innerHTML='New value';
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft+'px';
+var button=getElementById('counter');
+var counter=0;
+button.onClick=function(){
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
+    
 }
-img.onclick = function(){
-    var interval=setInterval(moveRight,50);
-    };
