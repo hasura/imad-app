5a304a6853769a962908e789d40fd953fa447782
Amusseus <30673952+Amusseus@users.noreply.github.com>
2017-08-08 22:53:16 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
- res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+ res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function (req, res) {
