6b82a67f361104d73036f1c1b9d6616866f33e7b
Amusseus <30673952+Amusseus@users.noreply.github.com>
2017-08-08 22:51:39 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,51 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+  title:'Article 1: Learning to use html is really simple!!',
+  heading:'Article One',
+  content:`<p>
+                html is one of the most simple syntax of any language that is interpreted by software.html is one of the most simple syntax of any language that is interpreted by software.html is one of the most simple syntax of any language that is interpreted by software.html is one of the most simple syntax of any language that is interpreted by software.html is one of the most simple syntax of any language that is interpreted by software.html is one of the most simple syntax of any language that is interpreted by software.html is one of the most simple syntax of any language that is interpreted by software.html is one of the most simple syntax of any language that is interpreted by software.html is one of the most simple syntax of any language that is interpreted by software.html is one of the most simple syntax of any language that is interpreted by software.
+            <p>
+            <p>
+                html is one of the most simple syntax of any language that is interpreted by software.html is one of the most simple syntax of any language that is interpreted by software.html is one of the most simple syntax of any language that is interpreted by software.html is one of the most simple syntax of any language that is interpreted by software.html is one of the most simple syntax of any language that is interpreted by software.html is one of the most simple syntax of any language that is interpreted by software.html is one of the most simple syntax of any language that is interpreted by software.html is one of the most simple syntax of any language that is interpreted by software.
+            <p>`
+};
+function createTemplate(data){
+var title = data.title;    
+var heading = data.heading;
+var content = data.content;
+var htmlTemplate = 
+  `<html>
+    <head>
+      <title>
+          ${title} 
+      </title>
+      <meta name="viewport" content="width-device-width,initial-scale-1"/>
+      <link href="/ui/style.css" rel="stylesheet"/>
+    </head>
+
+    <body>
+      
+        <h1>
+           ${heading}
+        </h1>
+        <div class="container">
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <div the cool paragraphs>
+            ${content}
+            </div>
+        </div>
+        
+        
+        
+    </body>
+</html>
+`;
+return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
