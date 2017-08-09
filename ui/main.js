19ec47b764707cdfd5ae69ed588574f9e6376852
JyothiRaju <jyothirajub@gmail.com>
2017-08-09 11:56:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log("Loaded");
 var element=document.getElementById('main-text');
 element.innerHTML='New value';
-var img=document.getElementById('img');
+var img=document.getElementById('madi');
 img.onclick = function(){img.style.marginLeft='100px';};
