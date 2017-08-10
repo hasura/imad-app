0cc41baa69156a028d5b0f5fb20ad0cbbc1fadc3
JyothiRaju <jyothirajub@gmail.com>
2017-08-10 13:31:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,5 +22,6 @@ submit.onclick=function(){
     for (var i=0;i<names.length;i++){
         list='<li>'+names[i]+'</li>';
         }
+        var ul=document.getElementById('namelist');
+        ul.innerHTML=list;
   };
-}
