47c92761a5d39041adf9b8d4606f135effc83151
JyothiRaju <jyothirajub@gmail.com>
2017-08-10 13:17:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,6 +16,6 @@ request.send(null);
 var nameInput=document.getElementById('name');
 var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
-submit.onClick=function{
+submit.onclick=function(){
 };
 }
