97ef78707206d4b77a65e82778ae8938ca34cb39
JyothiRaju <jyothirajub@gmail.com>
2017-08-10 13:27:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,5 +17,10 @@ var nameInput=document.getElementById('name');
 var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
-};
+    var names=['name1','name2','name3']
+    var list=' ';
+    for (var i=0;i<names.length;i++){
+        list='<li>'+names[i]+'</li>';
+        }
+  };
 }
