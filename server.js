3ef04ce078a544b88f5b6d5845fa4c99011574ad
varunwitty <30322599+varunwitty@users.noreply.github.com>
2017-08-11 22:00:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.get('/:articleName', function (req, res){
    res.send(createTemplate(articles[articleName]));
    });
 
-var articleName = req.params.arictleName;
+
 app.get('/ui/style.css', function (req, res) {
     var articleName = req.params.arictleName;
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
