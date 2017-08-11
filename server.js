38ffe737162f8dd11d12adfaf89d1ba4c1968706
varunwitty <30322599+varunwitty@users.noreply.github.com>
2017-08-11 22:46:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName', function (req, res){
-    var articleName = req.params.arictleName;
+    var articleName = req.params.articleName;
    res.send(createTemplate(articles[articleName]));
    });
 
