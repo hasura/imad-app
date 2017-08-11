761e6b221a09a54323b7821adfc9494d91cd30b6
mramanpvtGit <30836366+mramanpvtGit@users.noreply.github.com>
2017-08-11 15:13:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,8 +3,46 @@ var morgan = require('morgan');
 var path = require('path');
 
 var app = express();
-app.use(morgan('combined'));
 
+var pageContent = {
+    title: "iMad Web Appliation Example",
+    date: "11-Aug-2017",
+    header: "Main Page",
+    content: `<p>This is contents for Page 1 which is pretty long
+    and has several few lines of text, written into the server dot js file
+    as an object </p>
+    <p> This is another paragraph to show how paragraphs are formatted by html and how
+    they are distinguished </p>`
+};
+function makePage(data) {
+    var frontPageTemplate = `
+        <html>
+          <head>
+            <title> ${data.title} </title>
+            <link rel="stylesheet" href="ui/style.css">
+          </head>
+          <body>
+            <div class = "container">
+                <div>
+                    <a href="/", Home /a>
+                </div>
+                <br/>
+                 <h1>
+                    ${data.header}
+                 </h1>
+                <div>
+                    ${data.date}
+                </div>
+                <div>
+                    ${data.content}
+                </div>
+            
+            </div>
+          </body>
+        </html>
+        `;
+    return frontPageTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -17,6 +55,9 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('article-one', function (req,res) {
+    res.send(makePage(pageContent));
+});
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
