2a479d3cae4d0ae51bcb312da51326e7a9c15042
Dixon Selvan <dudedixon99@gmail.com>
2017-08-12 18:26:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,34 @@
 console.log('Loaded!');
 
-//var img = document.getElementById('drago');
+var img = document.getElementById('drago');
 var img1 = document.getElementById('dragoSounds');
+var leftmarg = 0;
+var rightmarg = 0;
+var count = 0;
+function moveDrago(){
+    if(leftmarg < 10){
+        leftmarg = leftmarg + 1;
+        img.style.marginLeft = leftmarg + 'px';
+    }
+    else if (leftmarg >= 10)
+    {
+        rightmarg = rightmarg + 1;
+        img.style.marginRight = rightmarg + 'px';
+        if(rightmarg >= 10)
+        {
+            leftmarg = 0;
+            rightmarg = 0;
+        }
+    }
+    else
+    {
+        leftmarg = 0;
+        rightmarg = 0;
+    }
+}
+img.onclick = function(){
+    var interval = setInterval(moveDrago, 1);
+};
 
 function Sound(source,volume,loop)
 {
