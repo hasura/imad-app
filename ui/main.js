5941a4f9ab56549884c63615a04270694094d7a5
Dixon Selvan <dudedixon99@gmail.com>
2017-08-12 17:52:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,10 +2,27 @@ console.log('Loaded!');
 
 var img = document.getElementById('drago');
 var leftmarg = 0;
-function moveright(){
-    leftmarg = leftmarg + 1;
-    img.style.marginLeft = leftmarg + 'px';
+var rightmarg = 0;
+function moveDrago(){
+    if(leftmarg < 10){
+        leftmarg = leftmarg + 1;
+        img.style.marginLeft = leftmarg + 'px';
+    }
+    else if (leftmarg == 10)
+    {
+        rightmar = rightmarg + 1;
+        img.style.marginRight = rightmarg + 'px';
+        if(rightmarg == 10)
+        {
+            leftmarg = 0;
+        }
+    }
+    else
+    {
+        leftmarg = 0;
+        rightmarg = 0;
+    }
 }
 img.onclick = function(){
-    var interval = setInterval(moveright, 50);
+    var interval = setInterval(moveDrago, 50);
 };
