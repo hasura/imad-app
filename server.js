571a40109c7fb6355c79505578d710284ce27cf9
Dixon Selvan <dudedixon99@gmail.com>
2017-08-12 14:43:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/article-one.html', function(req, res){
+app.get('/article-one.html', function(req, res){
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
