6f6b5e3e0b605612573892d968d36e2a7289a752
Dixon Selvan <dudedixon99@gmail.com>
2017-08-12 17:45:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,11 @@
 console.log('Loaded!');
+
+var img = document.getElementbyId('drago');
+var leftmarg = 0;
+function moveright(){
+    leftmarg = leftmarg + 1;
+    img.style.marginLeft = leftmarg + 'px';
+}
+img.onclick = function(){
+    var interval = setInterval(moveright, 50);
+};
