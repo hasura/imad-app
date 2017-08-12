47096a61417444266d60a7e8979583bfc3c261b1
Dixon Selvan <dudedixon99@gmail.com>
2017-08-12 18:03:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,29 +5,25 @@ var leftmarg = 0;
 var rightmarg = 0;
 var count = 0;
 function moveDrago(){
-    if(count === 0){
-        if(leftmarg < 10){
-            leftmarg = leftmarg + 1;
-            img.style.marginLeft = leftmarg + 'px';
-        }
-        else if (leftmarg >= 10)
-        {
-            rightmarg = rightmarg + 1;
-            img.style.marginRight = rightmarg + 'px';
-            if(rightmarg >= 10)
-            {
-                leftmarg = 0;
-                rightmarg = 0;
-            }
-        }
-        else
+    if(leftmarg < 10){
+        leftmarg = leftmarg + 1;
+        img.style.marginLeft = leftmarg + 'px';
+    }
+    else if (leftmarg >= 10)
+    {
+        rightmarg = rightmarg + 1;
+        img.style.marginRight = rightmarg + 'px';
+        if(rightmarg >= 10)
         {
             leftmarg = 0;
             rightmarg = 0;
         }
-        count = 1;
     }
-    count = 0;
+    else
+    {
+        leftmarg = 0;
+        rightmarg = 0;
+    }
 }
 img.onclick = function(){
     var interval = setInterval(moveDrago, 1);
