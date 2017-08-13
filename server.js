2a03fa8615d055626cb773eb60e43ae5dfa3280e
aravindan95 <30627162+aravindan95@users.noreply.github.com>
2017-08-13 19:49:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/articleone', function (req, res) {
 });
 
 app.get('/articletwo', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'incep.jpg'));
+  res.sendFile(path.join(__dirname, 'ui', 'interste.jpg'));
 });
 
 
