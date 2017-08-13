11ca6f2787888210f8f7ac0a93dc3e327c084b82
aravindan95 <30627162+aravindan95@users.noreply.github.com>
2017-08-13 19:32:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'https://www.qualcomm.co.in/products/', 'snapdragon'));
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
