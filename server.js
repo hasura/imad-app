5c60dde577f3dbcc6f2084b2279dd78287d61451
Lomitaswa Suna <lomitaswa@users.noreply.github.com>
2017-08-14 23:28:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,8 +82,8 @@ app.get('/ui/main.js', function ( req, res) {
 });
 
 
-app.get('/ui/madi.png', function ( req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/lomitaswa.png', function ( req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'lomitaswa.png'));
 });
 
 
