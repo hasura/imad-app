557eed7a36a4ed3bf87b1d275f8a7286c56862b5
Lomitaswa Suna <lomitaswa@users.noreply.github.com>
2017-08-14 20:31:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
 console.log('Loaded!');
+
+//Changing the text of the home page
+
+var element = document.getElementById('main')
+
+element.innerHTML = "Changes in the sever,but u can get the source codes in gitHub"
+
