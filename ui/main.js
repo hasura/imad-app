1a289d1ffafead5a49e5c4356849068a50ce4933
aravindan95 <30627162+aravindan95@users.noreply.github.com>
2017-08-14 21:58:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,12 @@
 console.log('Loaded!');
 var img=document.getElementById('nol')
 img
-img.onclick=function(){
-   var interval=setInterval(moveRight,50);
-};
 var marginLeft=0;
 function moveRight()
 {
     marginLeft=marginLeft+1;
     img.style.marginLeft-marginLeft + 'px';
 }
+img.onclick=function(){
+   var interval=setInterval(moveRight,50);
+};
