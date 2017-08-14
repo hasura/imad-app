97b46e36694f80fb65210780b2f18f5cf2126108
Lomitaswa Suna <lomitaswa@users.noreply.github.com>
2017-08-14 20:46:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,11 @@ element.innerHTML = 'Changes in the sever,but u can get the source codes in gitH
 //Moving the madi image
 
 var img = document.getElementById('madi');
-
+var marginLeft = 0;
+function moveRight() {
+    marginLeft = marginLeft + 5;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function() {
-    img.style.marginLeft = '100px';
+    var interval = setInterval(moveRight,50);
 }
