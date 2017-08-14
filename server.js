780959aca71bb9275a299cea524a3dd57721f566
chanduimad <30651009+chanduimad@users.noreply.github.com>
2017-08-15 00:23:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -35,7 +35,27 @@ var articles = {
     }
 };
 
-function createTemplate (data) {}
+function createTemplate (data) {
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    var htmlTemplate = `
+    <html>
+        <head>
+            <title>
+                ${title}
+            </title>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+        
+        </body>
+    </html>
+    `;
+    return htmlTemplate;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
