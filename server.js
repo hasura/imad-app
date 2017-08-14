3658fc1ba975621cd34e7ef6f7f61e8afb0081d9
Lomitaswa Suna <lomitaswa@users.noreply.github.com>
2017-08-14 11:56:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,23 +5,74 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/article-one', function (req, res){
-    res.sendFile(path.join(__dirname, 'ui','article-one.html'));
-});
+var articleOne = {
+	title: 'The Introduction | Web Articles',
+	heading: 'The Introduction',
+	content:`
+	 		<p>
+				New things can be added to this article...
+			</p>
+			<p>
+				Only some files can be added to this article that can be human readable rest of the files are for machine
+			</p>
+			<p>
+				Machine can read various type of file that are written by humans...<br />Continues in next article...
+			</p>
+		`
+}
+function createTemplate(data){
+	var title = data.title;
+	var heading = data.heading;
+	var content = data.content;
 
-app.get('/article-two', function (req, res){
-    res.sendFile(path.join(__dirname, 'ui','article-two.html'));
-});
+		var htmlTemplate =`
+			<html>
+			<head>
+				<title>
+					$[title]
+				</title>
+				<meta name="viewport" content="width_-device-width , initial-scale=1"/>
+				<link rel="stylesheet" type="text/css" href="/ui/style.css"/>
+			</head>
+			<body>
+				<div class="container_one">
+					<div>
+						<h1>
+							$[heading]
+						</h1>
+					</div>
+					<hr style="color: red">
+					<div class="holder_one">
+						$[content]
+					</div>
+				</div>
+			</body>
+			</html>
+		`;
+		return htmlTemplate;
+}
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+	res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
+app.get('/article-one',function ( req, res) {
+	res.send( createTemplate(articleOne));
+});
+app.get('/article-two',function ( req, res) {
+	res.sendFile(path.join(__dirname, 'ui','article-two.html'));
+});
+
+app.get('/article-three', function ( res, req){
+	res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+
+});
+
+app.get('/ui/style.css', function ( req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('/ui/madi.png', function ( req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
