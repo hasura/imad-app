07327517e38560885c54945b31166808d9cbd444
aravindan95 <30627162+aravindan95@users.noreply.github.com>
2017-08-15 22:03:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,11 +9,20 @@ function moveRight()
 }
 img.onclick=function() {
    var interval=setInterval(moveRight,50);
-};
-var counter=0;
+}
 var button=document.getElementById('counter');
 button.onclick=function(){
-    counter=counter+1;
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE)
+        if(request.status===200)
+        {
+    var counter=request.responseText;
     var span=document.getElementById('count');
     span.innerHTML=counter.toString();
-}
+        }
+    }
+ };
+ request.open('GET','http://aravindan2695.imad.hasura-app.io/counter',true);
+ request.send(null);
+ };
