3c92842c7e5abd031c96d2d63f3d47076d07d7d7
varunwitty <30322599+varunwitty@users.noreply.github.com>
2017-08-15 22:51:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,15 @@
-console.log('Loaded!');
+// counter code
+
+var bittuon = document.getElementById('counter');
+var counter = 0;
+buton.onclick=function () {
+  
+  // make a req to counter end point
+  
+  // capture a response and store it in a variable
+
+  // rendee the variable in the correct span 
+  counter = counter + 1;
+  var span = document.getElementById('count');
+  span.innerHTML = counter.toString();
+};
