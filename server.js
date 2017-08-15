1ca9266f96409b23e995bdd52c0960296dac1b27
aravindan95 <30627162+aravindan95@users.noreply.github.com>
2017-08-15 22:06:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+var counter=0;
+app.get('/counter', function (req, res) {
+  counter=counter+1;
+  request.send(counter.toString());
+});
 
 app.get('/articleone', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'articleone.html'));
