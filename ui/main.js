0dbe7a8f5257d9871d3b4a963f0451e7687b229c
mramanpvtGit <30836366+mramanpvtGit@users.noreply.github.com>
2017-08-16 16:29:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,8 +4,7 @@ button.onclick = function () {
     var name = document.getElementById('name');
     var inputName = name.value;
     
-    request.open('GET','http://mramanm.imad.hasura-app.io/submit-info?name=' + inputName, true);
-    request.send(null);
+
     request.onreadystatechange = function() {
         if ( request.readystate == XMLHttpRequest.DONE) {
             if ( request.status == 200 ) {
@@ -21,7 +20,8 @@ button.onclick = function () {
                 
         }
     };
-
+    request.open('GET','http://mramanm.imad.hasura-app.io/submit-info?name=' + inputName, true);
+    request.send(null);
 
     
 };
