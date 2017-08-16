88a5f363db68f671e2fb16a67d64ca58dc8ec772
vishalvarghese46 <30649220+vishalvarghese46@users.noreply.github.com>
2017-08-16 17:12:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function (req, res){
+    res.send('Article one will be served here');
+});
+
+app.get('/article-two', function (req, res){
+    res.send('Article two will be served here');
+});
+
+app.get('/article-three', function (req, res){
+   res.send('Article three will be served here'); 
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
