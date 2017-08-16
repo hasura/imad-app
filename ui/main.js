8f36f30ed37f2561e8b845fb4e0698403e3c451c
spsssankar <30971425+spsssankar@users.noreply.github.com>
2017-08-16 21:12:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,13 @@
 console.log('Loaded!');
 
 alert('hi how r u???');
+
+var img = document.getElementById('image');
+var mov=0;
+function movr() {
+    mov=mov + 10;
+    img.style.marginLeft=mov+'px';
+}
+img.onClick = function(){
+    var ele = setInterval(movr,50);
+};
