8f9fdc2655ce90ca468dec39ec76d26b2d5cecbe
Rohan Gupta <gupta.rohan1711@gmail.com>
2017-08-18 21:36:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('article-one',function(req,res)
+app.get('/article-one',function(req,res)
 {
    res.send('article one requested and will be served here'); 
 });
