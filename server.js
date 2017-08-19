7b1b07a98e0dc4f02ca2f3fa7f53f797ede60ad1
ChintamaniKoli <30986766+ChintamaniKoli@users.noreply.github.com>
2017-08-20 02:52:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function (req, res) {
