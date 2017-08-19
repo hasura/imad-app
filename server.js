cd17c40bbc3c2ea632a8a9f77bb64514db158dbf
suryawanshishan <31092110+suryawanshishan@users.noreply.github.com>
2017-08-19 20:04:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,6 +17,14 @@ app.get('/one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'one.html'));
 });
 
+app.get('/two', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'two.html'));
+});
+
+app.get('/three', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'three.html'));
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
