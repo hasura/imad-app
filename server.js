1ebc0a084de6ef7be8d80d2c31cecbc02813ba20
ChintamaniKoli <30986766+ChintamaniKoli@users.noreply.github.com>
2017-08-20 04:52:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,6 +85,11 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+
 app.get('/:articleName', function (req, res) {
 // articleName = article-one
 //articles[articleName] = {} content object for article one
@@ -92,13 +97,7 @@ var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article-two', function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
 
-app.get('/article-three', function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
