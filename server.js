b7768965a2f3cb8c019ca0a6334146cf619873ed
suryawanshishan <31092110+suryawanshishan@users.noreply.github.com>
2017-08-19 19:48:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/', function (req, res) {
+app.get('/one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'one.html'));
 });
 
