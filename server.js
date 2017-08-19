b7f39a1b9648f8c9274a8a6b1c7b3d1ca062e7bb
ChintamaniKoli <30986766+ChintamaniKoli@users.noreply.github.com>
2017-08-20 00:46:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,18 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/articl-one', function (req, res) {
+    res.send('Article One being requested and will be served here');
+});
+
+app.get('/articl-two', function (req, res) {
+    res.send('Article Two being requested and will be served here');
+});
+
+app.get('/articl-three', function (req, res) {
+    res.send('Article Three being requested and will be served here');
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
