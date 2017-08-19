bd24b03ab2f5f7f4581f873828310ef9757da633
ChintamaniKoli <30986766+ChintamaniKoli@users.noreply.github.com>
2017-08-20 02:48:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,36 +19,38 @@ var articleOne= {
                     This is the content for first article one and will be related to Introduction to Modern Application Development. Followed by Phyton Programming
                 </p>`
 };
-
-var htmlTemplate = `
-<html> 
-    <head> 
-        <title> 
-            ${title}
-        </title>
-        <meta name="viewport" content="width-device-width,intial-scale=1">
-      <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-    <body>
-        <div class="container">
-            <div>
-                <a href='/'>Home</a>
-            </div>
-            <hr/>
-            <h3>
-                ${heading}
-            </h3>    
-            <div>
-                ${date}
+function createTemplate(data) {
+    var htmlTemplate = `
+    <html> 
+        <head> 
+            <title> 
+                ${title}
+            </title>
+            <meta name="viewport" content="width-device-width,intial-scale=1">
+          <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="container">
+                <div>
+                    <a href='/'>Home</a>
+                </div>
+                <hr/>
+                <h3>
+                    ${heading}
+                </h3>    
+                <div>
+                    ${date}
+                </div>    
+                <div>
+                 ${content}
+                </div>
             </div>    
-            <div>
-             ${content}
-            </div>
-        </div>    
-    </body>
+        </body>
+    
+    </html>
+    `;
+}
 
-</html>
-`;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
