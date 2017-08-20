f598a368e0a3279a5b51dcffe49c41c6a4b93ebe
SakaSaiTrinath <sst.trinath@gmail.com>
2017-08-20 12:19:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ app.get('/', function (req, res) {
 
 app.get('/:articleName', function(req, res){
     //aticleName create the object as per the url typed by user.
-    var articleName = req.param.articleName;
+    var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
 
