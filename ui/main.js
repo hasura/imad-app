42a2b58e1c0732584d583262b91b1d097f3db0d6
ChintamaniKoli <30986766+ChintamaniKoli@users.noreply.github.com>
2017-08-20 05:41:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,8 +3,14 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerText = 'New Value';
+var marginLeft = 0;
 
+function moveRight ()
+{
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 var img = document.getElementById('madi');
 img.onclick = function () {
-  img.style.marginLeft = '150px';  
+  var interval = setInterval(moveRight,100);  
 };
