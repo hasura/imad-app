6624d40d45430e23df2763985e8413bbab4fd5d1
CHINTARAM <31130329+CHINTARAM@users.noreply.github.com>
2017-08-21 08:12:43 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool=require('pg').Pool;
+
+var config={
+    user:'chintaramvarma1',
+    database:'chintaramvarma1',
+    host:'db.io.hasura-app.io',
+    port:'5432'
+    password:process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool=new Pool(config);
+app.get('/test_db',function(req,res){
+    pool.query('SELECT * FROM test',function(err,result){
+        if(err){
+        res.status(500).send(err.toString());
+        }
+    else{
+        res.send(JSON.stringify(result));
+    }
+    });
 });
 
 app.get('/ui/style.css', function (req, res) {
