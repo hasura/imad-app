2aa13aada7a7ba44917a97ceb60c193c1b4fca98
Rohan Gupta <gupta.rohan1711@gmail.com>
2017-08-21 21:01:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,7 @@
-console.log('Loaded!');
+var counter=0;
+var button=document.getElementById('button');
+button.onclick=function(){
+    counter++;
+  var count=documnet.getElementById('count');
+  count.innerHTML=counter.toString();
+}
