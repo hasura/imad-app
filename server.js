98a2fa6a95fc735c6f8e5457f4beb4985b150859
sagarbhaltilak <31180744+sagarbhaltilak@users.noreply.github.com>
2017-08-21 21:17:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,58 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleone={
+    title:"article one by sagar bhaltilak",
+    heading:"article-one",
+    date:"aug 5 2017",
+    content:`
+    <p>
+            This is my first article and i am feeling great to do this.This is my first article and i am feeling great to do this.This is my first article and i am feeling great to do this.This is my first article and i am feeling great to do this.This is my first article and i am feeling great to do this.This is my first article and i am feeling great to do this.This is my first article and i am feeling great to do this.This is my first article and i am feeling great to do this.
+            </p>
+        <p>
+            This is my first article and i am feeling great to do this.This is my first article and i am feeling great to do this.This is my first article and i am feeling great to do this.This is my first article and i am feeling great to do this.This is my first article and i am feeling great to do this.This is my first article and i am feeling great to do this.This is my first article and i am feeling great to do this.This is my first article and i am feeling great to do this.
+        </p>
+        <p>
+            This is my first article and i am feeling great to do this.This is my first article and i am feeling great to do this.This is my first article and i am feeling great to do this.This is my first article and i am feeling great to do this.This is my first article and i am feeling great to do this.This is my first article and i am feeling great to do this.This is my first article and i am feeling great to do this.This is my first article and i am feeling great to do this.
+        </p>`};
+        
+        function createTemplate(data){
+            var title=data.title;
+            var date=data.date;
+            var heading=data.heading;
+            var content=data.content;
+        var htmlTemplate=`<!doctype html>
+<head>
+    <title>${title}</title>
+    <meta name="viewport" content="width-device-width intial-scale-1"/>
+<link href="/ui/style.css" rel="stylesheet" />
+</head>
+<body>
+    <div class="container">
+    <div>
+        <a href="/">home</a>
+    </div>
+    <hr/>
+    <h3>${heading}</h3>
+    <div>
+        ${date}
+    </div>
+    <div>
+        ${content}
+    </div>
+    </div>
+</body>
+</html>`;
+
+return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(req,res){
-res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+res.send(createTemplate(articleone));
 });
 
 app.get('/article-two',function(req,res){
@@ -37,3 +83,4 @@ var port = 80;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
+
