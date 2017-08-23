275fc4a280ccfb6ac5a563dca8eac098a7070f60
mramanpvtGit <30836366+mramanpvtGit@users.noreply.github.com>
2017-08-23 19:57:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.get('/submit-info', function (req,res) {
 var pool = new pg(config);
 
 app.get('/test-db', function(req,res){
-  pool.query('SELECT * FROM client_master', function (err, result){
+  pool.query("SELECT * FROM client_master", function (err, result){
      if ( err) {
          res.status(500).send(err.toString());
      } 
