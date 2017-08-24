4eb79a839b355f84c00212b4c0ad5506423301ec
sagarbhaltilak <31180744+sagarbhaltilak@users.noreply.github.com>
2017-08-24 11:39:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -67,12 +67,12 @@ app.get('/article-three',function(req,res){
 res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
+app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 
