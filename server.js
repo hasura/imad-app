83bef69ab17cb1a5bb7a6924e458ae5369d76c2e
mramanpvtGit <30836366+mramanpvtGit@users.noreply.github.com>
2017-08-24 14:48:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ app.get('/test-db', function(req,res){
          res.status(500).send(err.toString());
      } 
      else {
-         res.send(JSON.stringify(results));
+         res.send(JSON.stringify(result));
      }
   });
  //res.send(" I am alive for now!");
