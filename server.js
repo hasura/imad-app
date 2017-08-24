cf610da0cab7324bf9ee909b856227715522789a
H.SAI JEEVAN <31128541+hsaijeevan@users.noreply.github.com>
2017-08-24 11:33:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get(*article-one', function(req, res) {
+app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
