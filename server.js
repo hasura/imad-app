cc7d7e07b9b52bdd4eda647251338992ab1ce3c9
Rajeshnani123 <30943251+Rajeshnani123@users.noreply.github.com>
2017-08-25 08:10:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,12 +13,13 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
   
 });
-app.get('/article one', function(req,res)){
+app.get('/article one', function(req,res){
     res.send('this content is no more available');
 });
-app.get('/article two', function(req,res)){
+app.get('/article two', function(req,res){
     res.send('this content is not available');
-});app.get('/article three', function(req,res)){
+});
+app.get('/article three', function(req,res){
     res.send('this content is not available(manisivii kadhaa)');
 });
 }
