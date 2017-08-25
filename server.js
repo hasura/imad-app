9906fef871e700af071dcff0c887d5bb6e1ef891
Rajeshnani123 <30943251+Rajeshnani123@users.noreply.github.com>
2017-08-25 21:50:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,69 +2,6 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
-var articleOne= {
-    title :"article-one rajesh",
-    heading: "article one",
-    data: "sep,5,2016",
-    content:
-   ` <p>
-                       <li> who are you?</li>
-                        <li>what do u need from this?</li>
-                        <li>are you going to have an onine interview?</li>
-                        <li>if yes please go to other website.</li>
-    </p>            
-                     <p>
-                        <li> who are you?</li>
-                        <li>what do u need from this?</li>
-                        <li>are you going to have an onine interview?</li>
-                        <li>if yes please go to other website.</li>
-                    </p>
-                    <p>
-                       who are you?
-                        what do u need from this?
-                         are you going to have an onine interview?
-                        if yes please go to other website.
-    </p>`
-};
-function createTemplate(data)
-  var title=data.title;
-  var date=data.date;
-  var heading=data.heading;
-  var content=data.content
-
-var html Template=
-    `<html>
-     <head>
-         <title>
-                  S{title}
-         </title>
-          <meta name="viewport" content="width=device-width,initial-scale=1"/>
-          <link href="/ui/style.css" rel="stylesheet" />
-       </head>
-       <div class="centre">
-             <div>
-                 <a href="/">Home</a>
-             </div>
-         
-                 <h3>
-                     S{heading}
-                 </h3>
-                 <div>
-                 S{date}
-                 </div>
-                 <div>
- 
-                    S{content}
-                 
-                 </div>
-          </div>
-       </body>
-     
-</html>`
-;
-return html Template;
-}
-
 var app = express();
 app.use(morgan('combined'));
 
