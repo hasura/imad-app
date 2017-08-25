a8f214c78a7b8982b0aca63cbd5d9291aa74903c
Rajeshnani123 <30943251+Rajeshnani123@users.noreply.github.com>
2017-08-25 10:53:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,13 +13,13 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
   
 });
-app.get('/articleone', function(req,res){
+app.get('/article-one', function(req,res){
      res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
-app.get('/articletwo', function(req,res){
+app.get('/article-two', function(req,res){
      res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
-app.get('/articlethree', function(req,res){
+app.get('/article-three', function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 app.get('/ui/madi.png', function (req, res) {
