3a2e6e18356b1950de49888ed2ae6d010d7cf305
mramanpvtGit <30836366+mramanpvtGit@users.noreply.github.com>
2017-08-27 19:43:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var pg = require('pg').Pool;
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var app = express();
 
@@ -128,6 +129,20 @@ app.get('/verify', function (req,res) {
     	 }
     });
     
+});
+app.get('/check-login', function (req, res) {
+   if (req.session && req.session.auth && req.session.auth.userId) {
+       // Load the user object
+       pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
+           if (err) {
+              res.status(500).send(err.toString());
+           } else {
+              res.send(result.rows[0].username);    
+           }
+       });
+   } else {
+       res.status(400).send('You are not logged in');
+   }
 });
 app.get('/:articleName', function (req,res) {
     res.send(makePage(pageContents[req.params.articleName]));
