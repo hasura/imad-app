90e232f694c5e3aec4dc22cbc25c4493f188eb82
SakaSaiTrinath <sst.trinath@gmail.com>
2017-08-27 11:49:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg').Pool;
+
+var config = {
+    user: 'ssttrinath',
+    database: 'ssttrinath',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -110,6 +119,19 @@ var createTemplate = function(data) {
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+app.get('/test-db', function(req, res) {
+    //make a select request
+    //return a response with the results
+    pool.query('SELECT * FROM test', function(err, result){
+        if(err) {
+            res.status(500).send(err.toString());
+        } else {
+            res.send(JSON.Stringify(result));
+        }
+    });
 });
 
 app.get('/:articleName', function(req, res){
