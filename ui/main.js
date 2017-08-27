aaec398b20f40fcc43fad105cb1db93af2d74855
mramanpvtGit <mraman.m@gmail.com>
2017-08-27 19:51:32 +0530

    Displaying user list and login info

--- ui/main.js
+++ ui/main.js
@@ -1,55 +1,127 @@
-var button = document.getElementById('submit_button');
-function fill_names() {
-    
-    
-}
-button.onclick = function () {
 
-    var name = document.getElementById('name');
-    var inputName = name.value;
-    
+function displayUsername (username) {
+    var loginArea = `
+        <h3> Hi <i>${username}</i></h3>
+        <a href="/logout">Logout</a>
+    `;
+    document.getElementById('login_area').innerHTML = loginArea;
+}
+
+function verifyLogin () {
+    // Check if the user is already logged in
     var request = new XMLHttpRequest();
-    request.onreadystatechange = function() {
-        if ( request.readyState == XMLHttpRequest.DONE) {
-            if ( request.status == 200 ) {
-                var names = request.responseText;
-                names = JSON.parse(names);
-                var list = [];
-                for ( var i=0; i < names.length; i++ ) {
-                    list += '<li>' + names[i] + '</li>';
-                }
-                var ul = document.getElementById('namelist');
-                ul.innerHTML = list;
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                displayUser(this.responseText);
+            } else {
+                loadLoginForm();
             }
-                
         }
     };
-    request.open('GET','http://mramanm.imad.hasura-app.io/submit-info?name=' + inputName, true);
+    
+    request.open('GET', '/check-login', true);
     request.send(null);
-
+}
+function loadLoginForm () {
+    var loginHtml = `
+        <h3>Login/Register to unlock awesome features</h3>
+        <input type="text" id="username" placeholder="username" />
+        <input type="password" id="password" />
+        <br/><br/>
+        <input type="submit" id="login_button" value="Login" />
+        <input type="submit" id="register_button" value="Register" />
+        `;
+    document.getElementById('login_area').innerHTML = loginHtml;
     
-};
-var datareq = document.getElementById('data_button');
-
-datareq.onclick = function() {
+    // Submit username/password to login
+    var submit = document.getElementById('login_button');
+    submit.onclick = function () {
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  submit.value = 'Success!';
+              } else if (request.status === 403) {
+                  submit.value = 'Invalid credentials. Try again?';
+              } else if (request.status === 500) {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              } else {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              }
+              verifyLogin();
+          }  
+          // Not done yet
+        };
+        
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        request.open('POST', '/login', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        submit.value = 'Logging in...';
+        
+    };
     
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function() {
-        if ( request.readyState == XMLHttpRequest.DONE) {
-            if ( request.status == 200 ) {
-                var names = request.responseText;
-               names = JSON.parse(names);
-                var list = [];
-                var col = [];
-                for (var i = 0; i < names.length; i++) {
-                    for (var key in names[i]) {
-                        if (col.indexOf(key) === -1) {
-                             col.push(key);
-                         }
-                    }
-                } // extract Header information
-                // CREATE DYNAMIC TABLE.
-                var table = document.createElement("table");
+    var register = document.getElementById('register_button');
+    register.onclick = function () {
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  alert('User created successfully');
+                  register.value = 'Registered!';
+              } else {
+                  alert('Could not register the user');
+                  register.value = 'Register';
+              }
+          }
+        };
+        
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', '/create-user', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        register.value = 'Registering...';
+    
+    };
+}
+
+displayMembers = function(){
+	var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+    request.onreadystatechange = function () {
+		if (request.readyState === XMLHttpRequest.DONE) {
+			  // Take some action
+			  if (request.status === 200) {
+				var names = request.responseText;
+				names = JSON.parse(names);
+				alert('User list available');
+				var col = [];
+				for (var i = 0; i < names.length; i++) {
+					for (var key in names[i]) {
+						if (col.indexOf(key) === -1) {
+						 col.push(key);
+						}
+					}
+				} // extract Header information
+				var table = document.createElement("table");
 
                 // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
         
@@ -70,12 +142,18 @@ datareq.onclick = function() {
                     }
                 }
                 var divContainer = document.getElementById("table_data");
-                divContainer.innerHTML = "";
-                divContainer.appendChild(table);            }
-                
-        }
+                divContainer.innerHTML = "User List";
+                divContainer.appendChild(table);            
+			  }
+		      else {
+				  alert('Could not register the user');
+			  }
+		}
     };
-    request.open('GET','http://mramanm.imad.hasura-app.io/test-db', true);
-    request.send(null); 
-    
-};
+        
+        // Make the request
+    request.open('GET', '/verify', true);
+    request.send(null);  
+}
+verifyLogin();
+displayMembers();
