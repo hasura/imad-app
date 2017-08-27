d66380253d00b11808fae2c921ec1efa2ee32fa2
ApoorvaKashyap <30412013+ApoorvaKashyap@users.noreply.github.com>
2017-08-27 13:35:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,54 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var ArticleOne = {
+    title:'Article One|Apoorva Kashyap',
+    heading:'Article One',
+    date:'Aug 27, 20XX',
+    content:`
+     <p>
+        This is the content of the first article.This is the content of the first article.This is the content of the first article.This is the content of the first article.This is the content of the first article.This is the content of the first article.This is the content of the first article.This is the content of the first article.This is the content of the first article.
+    </p>
+    <p>
+        This is the content of the first article.This is the content of the first article.This is the content of the first article.This is the content of the first article.This is the content of the first article.This is the content of the first article.This is the content of the first article.This is the content of the first article.This is the content of the first article.
+    </p>
+    <p>
+        This is the content of the first article.This is the content of the first article.This is the content of the first article.This is the content of the first article.This is the content of the first article.This is the content of the first article.This is the content of the first article.This is the content of the first article.This is the content of the first article.
+    </p> `
+};
+
+function createTemp (data) {
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+    var htmlTemp = `
+        <html>
+            <head>
+                <title${title}</title>
+                <link href="/ui/style.css" rel="stylesheet" />
+                <meta name="viewport" content="width=device+width , initial-scale=1"/>
+            </head>
+            <body>
+                <div class=container>
+                <a href='/'>Home</a>&emsp;<a href='/ui/Article_Two.html'>Article Two</a>&emsp;<a href='/ui/Article_Three.html'>Article Three</a>
+                <hr>
+                <div>
+                <h1 align="center">${heading}</h1>
+                </div>
+                <div class=date>
+                    ${date}
+                </div>
+                <div>
+                    ${content}
+                </div>
+                </div>
+            </body>
+        </html>`;
+    return htmlTemp;
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -14,7 +62,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/ui/Article_One.html',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'Article_One.html'));
+    res.send(createTemp(ArticleOne));
 });
 
 app.get('/ui/Article_Two.html',function(req,res){
