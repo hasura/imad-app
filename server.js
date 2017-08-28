f5f1bf472c0c1986cf1eda60731022bfeb92a3d9
SakaSaiTrinath <sst.trinath@gmail.com>
2017-08-28 19:11:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -175,7 +175,7 @@ app.get('/test-db', function(req, res) {
 app.get('/articles/:articleName', function(req, res){
     //aticleName create the object as per the url typed by user.
     
-    pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "';", function(err, result){
+    pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName] , function(err, result){
        if(err) {
            res.status(500).send(err.toString());
        } else {
