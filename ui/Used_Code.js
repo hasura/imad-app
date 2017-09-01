de912099bb5b3fb319449d0c71b382c9f6bfccd3
ApoorvaKashyap <30412013+ApoorvaKashyap@users.noreply.github.com>
2017-09-01 19:57:21 +0530

    [imad-console] Updates ui/Used_Code.js

--- ui/Used_Code.js
+++ ui/Used_Code.js
@@ -1,1 +1,12 @@
+console.log('Loaded!');
 
+//To move MADI
+var img = document.getElementById("MADI");
+var marginLeft = 0;
+function moveRight(){
+  marginLeft += 5;
+  img.style.marginLeft = marginLeft+"px";
+ }
+img.onclick = function(){
+    var Interval = setInterval(moveRight , 100);
+};
