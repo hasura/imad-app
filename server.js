fa83d8ed33fe9a1a8c4d0462627973ab291a6b64
H.SAI JEEVAN <31128541+hsaijeevan@users.noreply.github.com>
2017-09-01 16:26:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,48 +1,29 @@
+var title = data.title;
+var heading = data.heading;
+var date = data.date;
+var content = data.content;
 
-<html lang="en">
+var htmltemplate =`
+<html>
 <head>
-  <title>JEEVAN IMAD profile</title>
-  <meta charset="utf-8">
-  <meta name="H.SAIJEEVAN" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="static/style.css">
+    <title>${title}</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link href="/ui/style.css" rel="stylesheet" />
 </head>
 <body>
-  <div class=9>
-    <div class="9">
-      <div class="col-sm-4 sidenav">
-        <div class="thumbnail">
-          <!-- Change the link in the src attribute below to an image available on the Internet -->
-          <img src="https://instagram.fbom4-1.fna.fbcdn.net/t51.2885-19/s150x150/19986035_479178559095616_954452302809268224_a.jpg" alt="Lights" style="width:100%" />
-        </div>
-        <h2 align="center"><b>H.SAI JEEVAN</b></h2>
-        <p align="center">NOTHING,<br>HASURA,<br>VISHAKPATNAM.</p>
-      </div>
-
-      <div class="col-sm-8 text-left">
-        <h3>I AM INTRESTED IN ACTING</h3>
-        <p>UPCOMMING ACTOR.</p>
-        <hr>
-        <h3>Education</h3>
-        <p> STILL STUDING 9</p>
-        <hr>
-        <h3>Experience</h3>
-        <p>My professional experience</p>
-        <hr>
-        <h3>A cool thing I've done recently</h3>
-        <p>WENT TO DAY AT SEA LAST SUNDAY<p>
-        <hr>
-         <a href="https://twitter.com/share" class="twitter-share-button" data-size="large" data-text="I just built #myfirstapp with #imad! @HasuraEd. Check it out here - " data-show-count="false">Tweet</a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
-         <iframe id="fb_share" src="" width="84" height="28" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
-         <script>
-            var current_url = window.location.href;
-            document.getElementById("fb_share").src="https://www.facebook.com/plugins/share_button.php?href="+current_url+"&layout=button_count&size=large&mobile_iframe=true&appId&width=84&height=28";
-         </script>
-      </div>
+    <div class="container">
+    <div> 
+        <a href="/">Home</a>
     </div>
-  </div>
-
-  <footer class="container-fluid text-center footer">
-    A little quote I like.
-  </footer>
+    
+    <hr/>
+    
+    <h3>Article One</h3>
+    
+    <div>${date}</div>
+    
+    <div>
+        ${content}
+    </div>
+    </div>
 </body>
-</html>
