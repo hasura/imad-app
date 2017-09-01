bfd330b2b38d2e828c76022d56254095880e7663
ApoorvaKashyap <30412013+ApoorvaKashyap@users.noreply.github.com>
2017-09-01 19:59:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,21 +1,10 @@
-/*console.log('Loaded!');
-
-//To move MADI
-var img = document.getElementById("MADI");
-var marginLeft = 0;
-function moveRight(){
-  marginLeft += 5;
-  img.style.marginLeft = marginLeft+"px";
- }
-img.onclick = function(){
-    var Interval = setInterval(moveRight , 100);
-};
-*/
-
 //Counter Code
+
 var button = document.getElementById('counter');
 var counter = 0;
+
 button.onclick = function() {
+ 
  
  counter=counter+1;
  var span = document.getElementById('count');
