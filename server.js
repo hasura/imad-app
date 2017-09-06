d532a20ad51a5fa2e0512c3a3b426ab56fc7ede9
H.SAI JEEVAN <31128541+hsaijeevan@users.noreply.github.com>
2017-09-06 16:25:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,29 +1,33 @@
-var title = data.title;
-var heading = data.heading;
-var date = data.date;
-var content = data.content;
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
 
-var htmltemplate =`
-<html>
-<head>
-    <title>${title}</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link href="/ui/style.css" rel="stylesheet" />
-</head>
-<body>
-    <div class="container">
-    <div> 
-        <a href="/">Home</a>
-    </div>
-    
-    <hr/>
-    
-    <h3>Article One</h3>
-    
-    <div>${date}</div>
-    
-    <div>
-        ${content}
-    </div>
-    </div>
-</body>
+var app = express();
+app.use(morgan('combined'));
+
+var articles = { 
+'articleone' : {
+title: 'Article One',
+heading: 'Article One',
+date: 'Aug 18 2017',
+content: <p>This is the paragraph tag.</p>
+<p>This is Another paragraph tag.</p>
+<p>This is One more paragraph tag.</p>
+},
+'articletwo' : {
+title: 'Article Two',
+heading: 'Article Two',
+date: 'Aug 20 2017',
+content: <p>This is the paragraph tag.</p>
+<p>This is Another paragraph tag.</p>
+<p>This is One more paragraph tag.</p>
+},
+'articlethree' : {
+title: 'Article Three',
+heading: 'Article Three',
+date: 'Aug 22 2017',
+content: <p>This is the paragraph tag.</p>
+<p>This is Another paragraph tag.</p>
+<p>This is One more paragraph tag.</p>
+}
+};
