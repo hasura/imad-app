aebc2d6b31d0945048732973c0983945bd8ede7e
H.SAI JEEVAN <31128541+hsaijeevan@users.noreply.github.com>
2017-09-06 16:50:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,29 +5,34 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articles = { 
-'articleone' : {
-title: 'Article One',
-heading: 'Article One',
-date: 'Aug 18 2017',
-content: <p>This is the paragraph tag.</p>
-<p>This is Another paragraph tag.</p>
-<p>This is One more paragraph tag.</p>
-},
-'articletwo' : {
-title: 'Article Two',
-heading: 'Article Two',
-date: 'Aug 20 2017',
-content: <p>This is the paragraph tag.</p>
-<p>This is Another paragraph tag.</p>
-<p>This is One more paragraph tag.</p>
-},
-'articlethree' : {
-title: 'Article Three',
-heading: 'Article Three',
-date: 'Aug 22 2017',
-content: <p>This is the paragraph tag.</p>
-<p>This is Another paragraph tag.</p>
-<p>This is One more paragraph tag.</p>
-}
-};
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get("/arcticle-one",function(req,res){
+    res.sendFile(path.join(__dirname, 'ui', 'arcticle-one.html'));
+});
+
+
+
+app.get("/arcticle-three",function(req,res){
+    res.sendFile(path.join(__dirname, 'ui', 'arcticle-three.html'));
+});
+
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+
+// Do not change port, otherwise your app won't run on IMAD servers
+// Use 8080 only for local development if you already have apache running on 80
+
+var port = 80;
+app.listen(port, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
