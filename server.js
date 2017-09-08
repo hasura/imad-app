845cfbc36728d23ebf27de3c9cc59deb43a1428b
H.SAI JEEVAN <31128541+hsaijeevan@users.noreply.github.com>
2017-09-08 16:01:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,14 +16,17 @@ app.get("/arcticle-one",function(req,res){
 
 
 app.get("/arcticle-tWO",function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'arcticle-three.html'));
+    res.send("article two requested and will be served here");
 });
 
 
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+  res.send("article three requestwd and will be served here");
 });
 
+app.get("/ui/style.css", function(req,res) {
+res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
