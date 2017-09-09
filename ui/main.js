ed2351f31825adba25eded1737ba5c7bad515fc3
SakaSaiTrinath <sst.trinath@gmail.com>
2017-09-09 16:46:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,8 +3,8 @@ console.log('Loaded!');
 //moving the image whenn click on it
 
 var img=document.getElementById("madi");
+var marginLeft =0;
 function moveRight() {
-    var marginLeft =0;
     marginLeft = marginLeft+10;
     img.style.margnLeft = marginLeft + "px";
 }
