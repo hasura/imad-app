dc10c492a655598150703614472097e25662f435
SakaSaiTrinath <sst.trinath@gmail.com>
2017-09-09 19:43:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,8 +100,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var names=[];
 app.get('/submit-name/:name', function(req, res) {
     var name = req.params.name;
+    names.push(name);
     res.send(JSON.stringify(name));
 });
 
