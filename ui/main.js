618f52b2e13d8198115cbe4367505bdd9404527f
SakaSaiTrinath <sst.trinath@gmail.com>
2017-09-09 16:43:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,8 +3,10 @@ console.log('Loaded!');
 //moving the image whenn click on it
 
 var img=document.getElementById("madi");
+function moveRight() {
+    marginLeft = marginLeft+10;
+    img.style.margnLeft = marginLeft + "px";
+}
 img.onclick = function() {
-    var x=100;
-  img.style.marginLeft= x+"px";  
-  x=x+100;
+    var interval = setInterval(moveRight,100);
 };
