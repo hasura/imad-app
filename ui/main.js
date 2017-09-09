e41316757d1a47ec97fa84a6e11a2e05e8ea5a16
SakaSaiTrinath <sst.trinath@gmail.com>
2017-09-09 20:12:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,3 +24,36 @@ button.onclick = function() {
   request.send(null);
   
 };
+
+
+submit.onclick = function() {
+   
+   var request = new XMLHttpRequest();
+  
+  
+  //capture the response and store in a variable
+  request.onreadystatechange = function() {
+      if(request.readyState === XMLHttpRequest.DONE){
+          //Take some action
+          if(request.status === 200) {
+              var names = request.responseText;
+              names = JSON.parse(names);
+              var ul = document.getElementById('list');
+              
+              for(var i=0;i<names.length;i++) {
+                  names[i] = '<li>' + names[i] + '</li>';
+              }
+              
+              ul.innerHTML=list;
+          }
+      }
+  };
+  
+  //make a request
+  request.open('GET', 'http://ssttrinath.imad.hasura-app.io/counter', true);
+  request.send(null);
+   
+   var nameInput = document.getElementById('name');  
+   var name = nameInput.value;
+   
+};
