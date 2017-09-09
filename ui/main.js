236b83379bbdb8c93e261825c33b4992bdc83f36
SakaSaiTrinath <sst.trinath@gmail.com>
2017-09-09 16:47:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ console.log('Loaded!');
 var img=document.getElementById("madi");
 var marginLeft =0;
 function moveRight() {
-    marginLeft = marginLeft+10;
+    marginLeft = marginLeft+50;
     img.style.margnLeft = marginLeft + "px";
 }
 img.onclick = function() {
