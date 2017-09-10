c98e8329473545a4d58edfc6139bfae8f3f28129
SakaSaiTrinath <sst.trinath@gmail.com>
2017-09-10 15:36:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
-var bodyParser = require('bodyParser');
+var bodyParser = require('body-parser');
 
 var config = {
     user: 'ssttrinath',
