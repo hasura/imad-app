d0e7705d45152002de834e81cd9aa6d199539e1f
SakaSaiTrinath <sst.trinath@gmail.com>
2017-09-10 17:22:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,29 +1,3 @@
-//counter code
-
-var button = document.getElementById('counter');
-button.onclick = function() {
-  
-  //create a new request
-  var request = new XMLHttpRequest();
-  
-  
-  //capture the response and store in a variable
-  request.onreadystatechange = function() {
-      if(request.readyState === XMLHttpRequest.DONE){
-          //Take some action
-          if(request.status === 200) {
-              var counter = request.responseText;
-              var span=document.getElementById('count');
-              span.innerHTML=counter.toString();
-          }
-      }
-  };
-  
-  //make a request
-  request.open('GET', 'http://ssttrinath.imad.hasura-app.io/counter', true);
-  request.send(null);
-  
-};
 
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
@@ -36,25 +10,25 @@ submit.onclick = function() {
       if(request.readyState === XMLHttpRequest.DONE){
           //Take some action
           if(request.status === 200) {
-              var names = request.responseText;
-              names = JSON.parse(names);
-              var list='';
-              
-              for(var i=0;i<names.length;i++) {
-                  list += '<li>' + names[i] + '</li>';
-              }
-              var ul = document.getElementById('namelist');
-              ul.innerHTML = list;
+              console.log('user logged in.');
+              alert('Loggedin successfully.');
+          } else if(request.status === 403) {
+              alert('Username/Password is incorrect!');
+          } else if(request.status === 500) {
+              alert('Something went wrong on the server side.');
           }
       }
   };
   
   //make a request
-   var nameInput = document.getElementById('name');  
-   var name = nameInput.value;
+   var username = document.getElementById('username');  
+   var password = document.getElementById('password');  
   
-  request.open('GET', 'http://ssttrinath.imad.hasura-app.io/submit-name?name='+name, true);
-  request.send(null);
+  request.open('POST', 'http://ssttrinath.imad.hasura-app.io/login', true);
+  request.setRequestHeader('Content-Type', 'application/json');
+  console.log(username);
+  console.log(password);
+  request.send(JSON.stringify({username: username, password: password}));
    
   
    
