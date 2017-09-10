5d8ecfde4a54f3bacdad4d167d74313a8ba54455
SakaSaiTrinath <sst.trinath@gmail.com>
2017-09-10 15:42:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get('/hash/:input', function(req, res) {
 app.get('/create-user', function(req, res) {
     //username, password
     //JSON
-    var salt = crypto.getRandomBytes(128).toString('hex');
+    var salt = crypto.randomBytes(128).toString('hex');
     var dbString = hash(password, salt);
     pool.query('INSERT INTO "user"(username, password) VALUES($1, $2)', [username, dbString]), function() {
         if(err) {
