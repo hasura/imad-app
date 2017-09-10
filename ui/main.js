e97e3d156f1f669aa7a2f1ef519b2c580f2bb478
ApoorvaKashyap <30412013+ApoorvaKashyap@users.noreply.github.com>
2017-09-10 10:39:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 //Counter Code
 
-var button = document.getElementById('button');
+/*var button = document.getElementById('button');
 var counter = 0;
 
 button.onclick = function() {
@@ -9,4 +9,12 @@ button.onclick = function() {
  counter=counter+1;
  var span = document.getElementById('no_of_times');
  span.InnerHTML = counter.toString();
-};
+};*/
+
+function clicked()
+{
+    counter=counter+1;
+    Document.write(counter);
+ var span = document.getElementById('no_of_times');
+ span.InnerHTML = counter.toString();
+}
