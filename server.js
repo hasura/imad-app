fc9909da9c53866cf73ba28ca1f17aa8c88facfa
SakaSaiTrinath <sst.trinath@gmail.com>
2017-09-10 20:04:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,9 +88,9 @@ app.post('/login', function(req, res){
 
 app.get('/check-login', function(req, res) {
    if(req.session && req.session.auth && req.session.auth.userId) {
-       res.send('you are logged in' );
+       res.send('yes' );
    } else {
-       res.send('you are not logged in');
+       res.send('no');
    }
 });
 
