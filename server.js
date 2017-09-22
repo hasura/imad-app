50bb6a9ad405371c588fc82037f7776fd2f8964b
vijay vasudevan <vijayvasudevan.uxd@gmail.com>
2017-09-22 19:03:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,15 +6,15 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+  res.sendFile(path.join(__dirname, 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  res.sendFile(path.join(__dirname, 'madi.png'));
 });
 
 
