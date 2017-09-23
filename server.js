61fa707d5e7537fd64addd52f025876a7200f516
vijay vasudevan <vijayvasudevan.uxd@gmail.com>
2017-09-23 15:35:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
