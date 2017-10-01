fa71cc25af02c336e97d1adeddb18e534355c30c
SarveshBajaj <sbsarveshbajaj1998@gmail.com>
2017-10-01 07:46:49 +0530

    articles

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
- 
+app.get('/article-one', function(req , res){
+  res.sendFile('Artice one requested and will be served')
+});
+app.get('/article-two', function(req , res){
+  res.sendFile('Artice one requested and will be served')
+});
+app.get('/article-three', function(req , res){
+  res.sendFile('Artice one requested and will be served')
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
@@ -21,7 +29,7 @@ app.get('/ui/madi.png', function (req, res) {
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
-var port = 80;
+var port = 8080;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
