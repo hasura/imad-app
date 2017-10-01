08374d4714dd189ed978e837bb94337b0497fa5c
SarveshBajaj <sbsarveshbajaj1998@gmail.com>
2017-10-01 08:26:14 +0530

    Unnecessary commit

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function(req , res){
-  res.sendFile('Artice one requested and will be served')
+  res.send('Artice one requested and will be served')
 });
 app.get('/article-two', function(req , res){
-  res.sendFile('Artice one requested and will be served')
+  res.send('Artice one requested and will be served')
 });
 app.get('/article-three', function(req , res){
-  res.sendFile('Artice one requested and will be served')
+  res.send('Artice one requested and will be served')
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
