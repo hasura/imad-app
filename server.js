e235364a1817a0ee726caf44d3890a41a5ff1f65
JyothiRaju <jyothirajub@gmail.com>
2017-11-10 14:04:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express=require('express');
 var morgan=require('morgan');
 var path=require('path');
+var app=express();
 app.get('/',function(req,res){
     res.sendFile(path,join(__dirname,'ui','index.html'));
 });
