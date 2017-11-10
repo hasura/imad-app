9ac3c92540662ec941c9cc3b5d38b919f373ef3d
JyothiRaju <jyothirajub@gmail.com>
2017-11-10 13:54:44 +0530

    [imad-console] Updates ui/server.js

--- ui/server.js
+++ ui/server.js
@@ -1,3 +1,9 @@
+var express=require('express');
+var morgan=require('morgan');
+var path=require('path');
+app.get('/',function(req,res){
+    res.sendFile(path,join(__dirname,'ui','index.html'));
+});
 app.get('ui/style.css',function(req,res){
-    res.sendFile(path,join(__dirname,'ui','style.css'))
+    res.sendFile(path,join(__dirname,'ui','style.css'));
 });
