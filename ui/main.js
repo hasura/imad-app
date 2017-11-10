3408f8abf36fe03b2d61e3f4ea10ca111ec8cf61
JyothiRaju <jyothirajub@gmail.com>
2017-11-10 12:44:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,15 @@
 var button=document.getElementById('counter');
 var counter=0;
-button.onclick=function(){
-    var request=new XMLHttpRequest();
-    request.onreadystatechange=function(){
-        if (request.readyState===XMLHttpRequest.DONE){
-            if (request.status==200)
-                var counter=request.responseText;
+button.onclick=function(req,res){
+    req=new XMLHttpRequest();
+    req.onreadystatechange=function(){
+        if (req.readyState==XMLHttpRequest.DONE){
+            if (req.status==200)
+                var counter=req.responseText;
                 var span=document.getElementById('count');
                 span.innerHTML=counter.toString();
 }
-}
-}
+};
+};
 request.open('GET','http://jyothirajub.imad.hasura-app.io/counter',true);
-request.send(null);
-}
+request.send(res);
