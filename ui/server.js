faa157b5bb9381c04a9c762bc996a1d50dce5919
JyothiRaju <jyothirajub@gmail.com>
2017-11-13 11:46:28 +0530

    [imad-console] Updates ui/server.js

--- ui/server.js
+++ ui/server.js
@@ -10,4 +10,6 @@ app.get('/ui/style.css',function(req,res){
     res.sendFile(path,join(__dirname,'ui','style.css'));
 });
 var port=8080;
-app.listen(8080,function(){console.log('IMAD is listnening to  port "${port}"');});
+app.listen(8080,function(){
+    console.log('IMAD is listnening to  port "${port}"');
+});
