fda767c03126038b25e4d22a0dff86593a210caa
JyothiRaju <jyothirajub@gmail.com>
2017-11-13 11:34:23 +0530

    [imad-console] Updates ui/server.js

--- ui/server.js
+++ ui/server.js
@@ -3,10 +3,10 @@ var morgan=require('morgan');
 var path=require('path');
 var app=express();
 app.use(morgan('combined'));
-app.get('ui/index.html',function(req,res){
+app.get('/',function(req,res){
     res.sendFile(path,join(__dirname,'ui','index.html'));
 });
-app.get('ui/style.css',function(req,res){
+app.get('/ui/style.css',function(req,res){
     res.sendFile(path,join(__dirname,'ui','style.css'));
 });
 var port=8080;
