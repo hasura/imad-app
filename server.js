838edfd295c3099e580524399e2ab34a417f07b4
pandiri1991 <32785783+pandiri1991@users.noreply.github.com>
2017-12-05 22:18:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+app.get('/maithin me', function (req, res) {
+res.send('maithin requsted and will be served here' );
 });
 
 app.get('/ui/madi.png', function (req, res) {
