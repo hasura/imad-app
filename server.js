a250273dec522c2fcdf6cf537e025456f9058e04
pandiri1991 <32785783+pandiri1991@users.noreply.github.com>
2017-12-15 18:17:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 
-var port = 80;
+var port = 8080;
 app.listen(80, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
