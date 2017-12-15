d214cffdc066b7cdfb13a334965b0f0372b40a3d
pandiri1991 <32785783+pandiri1991@users.noreply.github.com>
2017-12-15 17:50:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,20 @@ app.get('/', function (req, res) {
   res.sendfile(path.join(__dirname, 'ui', 'index.htal'));
 });
 
-app.get('/article-one', function (req, res) {
+app.get('/Article-one', function (req, res) {
     res.send('Article one requested and will be served here');
+});
+
+app.get('/Article-two', function (req, res) {
+    res.send('Article two requested and will be served here');
+});
+
+app.get('/Article-three', function (req, res) {
+    res.send('Article three requested and will be served here');
+});
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendfile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
