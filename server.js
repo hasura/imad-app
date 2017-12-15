aa0a66ce06965d809f6d346f563900894bad93f9
pandiri1991 <32785783+pandiri1991@users.noreply.github.com>
2017-12-15 17:23:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,8 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-app.get('/maithin me', function (req, res) {
-res.send('maithin requsted and will be served here' );
+app.get('/article-one', function (req, res) {
+    res.send('Article one requested and will be served here');
 });
 
 app.get('/ui/madi.png', function (req, res) {
