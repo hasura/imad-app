0cce38f39afecdd75c96c0e137fa01323f52a097
rajat <rajatkantinandi1993@gmail.com>
2018-02-14 22:38:22 +0530

    Edited server.js to accept any resource inside /ui/* path using one id parameter

--- server.js
+++ server.js
@@ -9,12 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/:id', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', req.params.id));
 });
 
 
