f8060e2ab28ec6104012a6bc75b6e2f70f54cc18
noragami <yato@noragamis-MacBook-Pro.local>
2018-02-17 23:07:20 +0530

    Added article-two

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/article-one',function(req, res) {
 });
 
 app.get('/article-two',function(req, res) {
-    res.send('article two resquested will be served here')
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three',function(req, res) {
