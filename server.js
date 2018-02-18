b572bcca6e312a7d0434fe7938878a7aadecc3a6
parmeet1402 <33989504+parmeet1402@users.noreply.github.com>
2018-02-18 22:08:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,6 +102,9 @@ app.get('/:articleName', function (req, res) {
 });
 
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 
 
 // Do not change port, otherwise your app won't run on IMAD servers
