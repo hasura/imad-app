ca148b77be13f08a82884834988b52b86e78613c
parmeet1402 <33989504+parmeet1402@users.noreply.github.com>
2018-02-18 22:31:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,11 @@ element.innerHTML="New Value";
 
 //hiding the image
 var picture1= document.getElementById('madi');
-
+var marginLeft=0;
+function moveRight(){
+    marginLeft= marginLeft+10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 picture1.onClick = function(){
-    picture1.style.marginLeft= '100px';
+    var interval= setInterval(moveRight, 100);
 }
