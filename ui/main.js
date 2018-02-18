ec7450a5e6bb39d2753dacafe6e03d1f3b71d547
parmeet1402 <33989504+parmeet1402@users.noreply.github.com>
2018-02-18 22:23:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,8 @@ console.log('Loaded!');
 //change contents of an element
 var element= document.getElementById('main-text');
 element.innerHTML="New Value";
+
+
+//hiding the image
+var picture1= document.getElementById('madi');
+picture1.src=" ";
