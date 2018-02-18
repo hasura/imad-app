be600b842a1de1d9096e068b2b2a68a49d40236a
akashc777 <akashc777@gmail.com>
2018-02-18 22:26:26 +0530

    removed style

--- server.js
+++ server.js
@@ -39,17 +39,9 @@ function createTemplate (data) {
       <title>
         ${title}
       </title>
-      <meta name="viewport" content="width=device-width, intial">
+      <meta name="viewport" content="width=device-width, intial-scale=1">
       <link href="/ui/style.css" rel="stylesheet" />
-      <style>
-          .container{
-              max-width: 800px;
-              margin: 0 auto;
-              color: grey;
-              font-family: sans-serif;
-
-          }
-      </style>
+      
     </head>
     <body>
       <div class = "container">
@@ -100,7 +92,7 @@ app.get('/ui/madi.png', function (req, res) {
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
-var port = 80;
+var port = 8080;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
