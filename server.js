6de92b67159d532f54f77810a935c9869acb3b14
parmeet1402 <33989504+parmeet1402@users.noreply.github.com>
2018-02-19 20:10:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,11 +85,6 @@ app.get('/', function (req, res) {
 });
 
 //counter 
-var counter=0;
-app.get('/counter', function(req,res) {
-    counter=counter+1;
-    res.send(counter.toString());
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
@@ -102,6 +97,11 @@ app.get('/ui/madi.png', function (req, res) {
 
 
 //--------user changes-----------
+var counter=0;
+app.get('/counter', function(req,res) {
+    counter=counter+1;
+    res.send(counter.toString());
+});
 
 app.get('/:articleName', function (req, res) {
     var articleName=req.params.articleName;
