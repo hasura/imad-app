f07667f99de9118ad8cd78a90b48301913837c3b
parmeet1402 <33989504+parmeet1402@users.noreply.github.com>
2018-02-20 01:51:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,13 +16,34 @@ console.log('Loaded!');
   //  var interval= setInterval(moveRight, 100);
 //};
 
+var btn=document.getElementById('counter--button');
+btn.onclick=function(){
+
+    //create a request object
+    var request=new XMLHttpRequest();
+
+    //capture the response and store it in a variable
+    requeset.onreadystatechange = function(){
+        if(request.readyState ===  XMLHttpRequest.DONE) {
+            //take some action
+            if(request.status === 200){
+                 var counter = request.responseText;
+                 var span=document.getElementById('count');
+                 span.innerHTML = counter.toString();
+                
+            }
+        }
+        
+        //not done yet
+    }
+
+    request.open('GET','http://parmeetasi.imad.hasura-app.io/counter',true);
+    request.send(null);
+};
+
+
 
 
 //counter button
 var counter=0;
-var btn=document.getElementById('counter--button');
-btn.onclick=function(){
     counter++;
-    var span=document.getElementById('count');
-    span.innerHTML = counter.toString();
-};
