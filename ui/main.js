b687ae4deff779d1a05f06ca72a6a3fb006fe96e
parmeet1402 <33989504+parmeet1402@users.noreply.github.com>
2018-02-20 02:37:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,6 +19,8 @@ console.log('Loaded!');
 
 //create the counter button and making ajax call
 
+var nameInput= document.getElementById('name');
+var name= nameInput.value;
 var btn=document.getElementById('counter--button');
 btn.onclick=function(){
 
@@ -30,17 +32,23 @@ btn.onclick=function(){
         if(request.readyState ===  XMLHttpRequest.DONE) {
             //take some action
             if(request.status === 200){
-                 var counter = request.responseText;
-                 var span=document.getElementById('count');
-                 span.innerHTML = counter.toString();
-                
+          //capture a list of names and render it as a list
+            var names = request.responseText;
+            names=JSON.parse(names);
+            var list='';
+            for (var i=0;i<names.length;i++){
+                list += '<li>'+names[i]+'</li>';
+            }
+            
+            var ul=document.getElementById('namelist');
+            ul.innerHTML=list;
             }
         }
         
         //not done yet
     };
 
-    request.open('GET','http://parmeetasi.imad.hasura-app.io/counter',true);
+    request.open('GET','http://parmeetasi.imad.hasura-app.io/submit-name?name'+name,true);
     request.send(null);
 };
 
@@ -53,18 +61,8 @@ btn.onclick=function(){
 
 
 //submit name
-var nameInput= document.getElementById('name');
-var name= nameInput.value;
 var submit= document.getElementById('submit--btn');
 submit.onclick = function(){
     //make a request to the server and send the name
-    //capture a list of names and render it as a list
-    var names=['name1','name2','name3','name4'];
-    var list='';
-    for (var i=0;i<names.length;i++){
-        list += '<li>'+names[i]+'</li>';
-    }
-    
-    var ul=document.getElementById('namelist');
-    ul.innerHTML=list;
+  
 };
