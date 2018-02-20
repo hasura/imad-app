da29de59e9ac400a98a606c29fe24631273118ee
dhirajwork <dhiraj.jaiswal96@gmail.com>
2018-02-20 20:37:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/', function (req, res) {
-  res.send(path.join(__dirname, 'ui', 'guess.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'guess.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
