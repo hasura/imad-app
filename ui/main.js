796f0087fb9c364264aee49b0d0beaad4da4bfcc
akashc777 <akashc777@gmail.com>
2018-02-20 20:45:38 +0530

    fixed errors in main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@ button.onclick = function(){
 
     if(request.readyState === XMLHttpRequest.DONE) {
 
-      if(request.status === 502){
+      if(request.status === 200){
         var counter = request.responseText;
         var span = document.getElementById('count');
         span.innerHTML = counter.toString();
