5620ae022dc30940a45287ca7edd62bf001c081d
dhirajwork <dhiraj.jaiswal96@gmail.com>
2018-02-20 20:34:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'guess.html'));
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
