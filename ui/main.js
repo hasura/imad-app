b3b88457d7bc057587966fe72dcf8c3874b60a4d
parmeet1402 <33989504+parmeet1402@users.noreply.github.com>
2018-02-20 14:31:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -49,6 +49,6 @@ submit.onclick=function(){
 
     var nameInput= document.getElementById('name');
     var name= nameInput.value;
-    request.open('GET','http://parmeetasi.imad.hasura-app.io/submit-name?name'+name,true);
+    request.open('GET','http://parmeetasi.imad.hasura-app.io/submit-name?name='+name,true);
     request.send(null);
 };
