52cbbb10926a542cc322bb018811ee22af12b76e
akashc777 <akashc777@gmail.com>
2018-02-21 22:02:03 +0530

    added /submit-name qury

--- server.js
+++ server.js
@@ -107,9 +107,14 @@ function createTemplate (data) {
     `;
   return htmlTemplate;
 
-}
+};
 
-
+var names = [];
+app.get('/submit-name', function (req, res) {
+  var name = req.query.name;
+  names.push(name);
+  res.send(JSON.stringify(names));
+});
 
 app.get('/:articleName',function(req, res) {
     var articleName = req.params.articleName;
@@ -130,6 +135,8 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+
+
 
 
 // Do not change port, otherwise your app won't run on IMAD servers
--- ui/main.js
+++ ui/main.js
@@ -17,6 +17,38 @@ button.onclick = function(){
       }
   };
 
-  request.open('GET','http://akashc777.imad.hasura-app.io/counter',true);
-  request.send(null)
+  request.open('GET','http://localhost:8080/counter',true);
+  request.send(null);
 };
+
+
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+
+  var nameInput = document.getElementById('name');
+  var name = nameInput.value;
+    var request = new XMLHttpRequest();
+
+    request.onreadystatechange = function () {
+
+      if(request.readyState === XMLHttpRequest.DONE) {
+
+        if(request.status === 200){
+
+          var names = request.responseText;
+          names = JSON.parse(names);
+          var list = '';
+          for (var i=0; i<names.length;i++){
+            list += '<li>' + names[i] + '</li>';
+
+          }
+          var ul = document.getElementById('namelist');
+          ul.innerHTML = list;
+          }
+        }
+    };
+
+    request.open('GET','http://localhost:8080/submit-name?name=' + name,true);
+    request.send(null);
+
+} ;
