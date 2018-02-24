277025b6a6598812daabd5c6ae30141e3a308dd8
CoderOO7 <shubhamyadav.70007@gmail.com>
2018-02-23 18:03:11 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,50 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var content = {
+    title: 'Dream Diary',
+    date: '24 Februaury 2018',
+    heading2: 'Dreams Become true',
+    content:
+        `<p>                                
+            <h3>Hello Everyone</h3><br>
+             Myself Shubham a computer geek and tech enthuastic.I am 
+             2nd year CSE student in Dronacharya College of Engineering.And today on 24 
+             February 2018 I am trying to create my dreams diary online.Well see how far I am
+             able to make my dream dairy attractive and beautiful.<br>I hope you will enjoy
+             it. For review my dream diary click on the link below.
+        </p>`
+};
+
+function createTemplate(object){ 
+    var title = data.title;
+    var date = data.title;
+    var content = data.content;
+    var htmlTemplate = `
+    <html>
+      <head>
+         <title>${title}</title>
+         <meta name="viewport" content="width=device-width, initial-scale=1" />
+         <link href="/ui/style.css" rel="stylesheet"/>
+      </head>
+      
+      <body>
+        <div class="container">
+          <div class="navBar">
+            <h2>${heading2}</h2>
+            <a href='/'>Home</a>
+            <a href='/'>Dreams list</a>
+          </div>
+          <hr/>
+    
+          <div class="box">
+            ${content}
+          </div>
+        </div>
+      </body>
+    </html>
+    `;
+}
 app.get('/ui/index', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 }); 
