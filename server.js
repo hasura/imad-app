6e56016c87526cfed46b5d6dd52e10b698fac2fd
CoderOO7 <shubhamyadav.70007@gmail.com>
2018-02-24 15:34:08 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ app.get('/girlfriend',function(req,res){
 });
 
 app.get('/ui/main.js',function(req, res){
-    res.sendFile(path.join(_dirname, 'ui', 'main.js'));
+    res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
