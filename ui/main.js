162168179c7eca18290125b16a7f2504a90bd6b9
CoderOO7 <shubhamyadav.70007@gmail.com>
2018-02-24 15:49:05 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 
 //writinG new content to index.html
 
-var content = document.getElementnyById(mian-content);
+var content = document.getElementnyById(main-content);
 
 element.innerHtml = 'Hello CoderOO7(WEB DEVELOPER)';
 
