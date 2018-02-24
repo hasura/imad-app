7e455559d4da4f5db0099766cccbade0130728e4
srilakshmii <610sssg@gmail.com>
2018-02-24 21:00:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,6 @@ var element=document.getElementById('a');
 
 element.innerHTML='okay';
 
-var img=document.getElementById('c').src;
+var img=document.getElementById('c');
 var oldimg=document.getElementById('b');
 oldimg.innerHTML=img;
