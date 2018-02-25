440b7aa42e951f153c328ce05d434a7daf9c96da
CoderOO7 <shubhamyadav.70007@gmail.com>
2018-02-24 23:58:24 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,9 @@
 console.log('Loaded!');
 
 //writinG new content to index.html
-
+ 
 var content = document.getElementnyByid(main-content);
 
+alert("hi I'm CoderOO7");
 element.innerHtml = 'Hello CoderOO7(WEB DEVELOPER)';
 
