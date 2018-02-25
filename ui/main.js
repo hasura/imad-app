2ba02ee7d4fd5a5296f6fdc1d89875432e50bd9f
CoderOO7 <shubhamyadav.70007@gmail.com>
2018-02-25 06:21:55 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,13 @@
 //Move the image
 
+var marginLeft = 0;
+function moveRight() {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
+
 var img = document.getElementById('coder');
-coder.onclick = function() {
-    img.style.marginLeft = '100px';
+img.onclick = function() {
+    var interval = setInterval(moveRight,100);
 }
 
