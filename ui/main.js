d367c9547980fa5e819ff5cf02eed60b9e8fd79f
CoderOO7 <shubhamyadav.70007@gmail.com>
2018-02-25 00:34:49 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,3 +8,10 @@ alert("hi I'm CoderOO7");
 
 element.innerHTML = 'Hello CoderOO7(WEB DEVELOPER)';
 
+//Move the image
+
+var img = document.getElementById('madi');
+log.onclick = function() {
+    log.style.marginleft = '100px';
+}
+
