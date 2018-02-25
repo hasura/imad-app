34a9078f10c7c13ef7a249099ca35917c0a33498
CoderOO7 <shubhamyadav.70007@gmail.com>
2018-02-25 04:30:48 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -63,6 +63,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 }); 
 
+var counter = 0;
+app.get('/counter', function (req, res) {
+    counter = counter + 1;
+    res.send(counter.toString());
+});
+
 app.get('/:dreamName', function(req, res) {
     //dreamName = dream1
     //dream[dreamName] = dream[dream1]
