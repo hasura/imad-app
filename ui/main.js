cd341816026054e4e7e77a70f463565c0872938b
CoderOO7 <shubhamyadav.70007@gmail.com>
2018-02-25 07:32:32 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,11 @@
 //Counter Code
 var button = document.getElementById('counter');
+var counter = 0;
 button.onclick = function() {
+    
+    //capture the response and store it in a variable
+    
+    
     //render the variable in correct span
     counter = counter + 1;
     var span = document.getElementById('count');
