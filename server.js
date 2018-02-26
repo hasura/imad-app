5f2c33246d6b80adc4b43d48ae844cc7e3c01e9a
parmeet1402 <33989504+parmeet1402@users.noreply.github.com>
2018-02-27 02:46:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,6 +96,14 @@ var pool=new Pool(config);
 app.get('/test-db',function(req,res){
    //make a select request 
    //return a response with results
+   
+   pool.query('SELECT * FROM user', function(err, result){
+       if(err){
+           res.status(500).send(err.toString());
+       }else{
+            res.send(JSON.stringify(result));
+       }
+   })
 });
 
 
