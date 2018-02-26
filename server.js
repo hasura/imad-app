bd776ae07c4c5c7315c424dd348e1875be5b77c1
parmeet1402 <33989504+parmeet1402@users.noreply.github.com>
2018-02-27 03:38:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -150,10 +150,10 @@ app.get('/articles/:articleName', function (req, res) {
                 res.status(404).send("article not found");
             }else{
                 var articleData = result.rows[0];
+                res.send(createTemplate(articleData));
             }
         }
     });
-  res.send(createTemplate(articleData));
 });
 
 
