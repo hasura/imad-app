7cceec37593890aafb9c8ac7adf4639d15c6cc67
parmeet1402 <33989504+parmeet1402@users.noreply.github.com>
2018-02-27 02:38:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require ('pg').Pool;
 
+var config={
+    user: 'parmeetasi',
+    database: 'parmeetasi',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: 
+};
 var app = express();
 app.use(morgan('combined'));
 
@@ -83,6 +91,12 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+app.get('/test-db',function(req,res){
+   //make a select request 
+   //return a response with results
+});
+
 
 //counter 
 var counter=0;
