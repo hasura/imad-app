41467d8cf172974b3eba0b8a3c9b2e57808c13db
parmeet1402 <33989504+parmeet1402@users.noreply.github.com>
2018-02-27 03:40:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -142,7 +142,7 @@ app.get('/ui/madi.png', function (req, res) {
 app.get('/articles/:articleName', function (req, res) {
     var articleName=req.params.articleName;
     
-    pool.query("SELECT * FROM article WHERE title ="+req.params.articleName,function(err, result){
+    pool.query("SELECT * FROM article WHERE title = '"+req.params.articleName+"'",function(err, result){
         if(err){
             res.status(500).send(err.toString());
         }else{
