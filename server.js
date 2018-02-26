75befb2be9fd4a28aa2d5504123120045700a6e0
parmeet1402 <33989504+parmeet1402@users.noreply.github.com>
2018-02-27 02:52:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require ('pg').Pool;
-
+ 
 var config={
     user: 'parmeetasi',
     database: 'parmeetasi',
