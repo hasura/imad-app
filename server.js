cca5cea574be4d3d76e1470a310a7e14c1b9b3ab
parmeet1402 <33989504+parmeet1402@users.noreply.github.com>
2018-02-27 02:59:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config={
     database: 'parmeetasi',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: process.emv.DB_PASSWORD
+   // password: process.emv.DB_PASSWORD
 };
 var app = express();
 app.use(morgan('combined'));
