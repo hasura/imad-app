55837c72241d5a92e7592325a57718fc417ea8be
parmeet1402 <33989504+parmeet1402@users.noreply.github.com>
2018-02-27 03:22:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -139,9 +139,21 @@ app.get('/ui/madi.png', function (req, res) {
 
 //--------user changes-----------
 
-app.get('/:articleName', function (req, res) {
+app.get('/articles/:articleName', function (req, res) {
     var articleName=req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
+    
+    pool.query("SELECT * FROM article WHERE title ="+req.params.articleName,function(err, result){
+        if(err){
+            res.status(500).send(err.toString());
+        }else{
+            if(result.row.length === 0){
+                res.status(404).send("article not found");
+            }else{
+                var articleData = result.rows[0];
+            }
+        }
+    });
+  res.send(createTemplate(articleData));
 });
 
 
