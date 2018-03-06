91c147169f04c4415a1596bfe992cfb5c6bf3546
parmeet1402 <33989504+parmeet1402@users.noreply.github.com>
2018-03-07 02:20:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require ('pg').Pool;
- 
+var crypto = require('crypto');
 var config={
     user: 'parmeetasi',
     database: 'parmeetasi',
@@ -55,6 +55,23 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+
+function hash(input, salt){
+    //how do we create the hash
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return hashed;
+}
+
+
+
+app.get('/hash/:input', function(req,res){
+   var hashedString = hash(req.params.input, "this_is_random_string");
+   res.send(hashedString);
+});
+
+
+
 
 var pool=new Pool(config);
 app.get('/test-db',function(req,res){
