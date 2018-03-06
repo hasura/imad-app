3cfaf2a4eedcc9eb5ad9e3b4125b87b1f803df44
parmeet1402 <33989504+parmeet1402@users.noreply.github.com>
2018-03-07 04:03:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -115,7 +115,7 @@ app.post('/login', function(req,res){
     
     
     var dbString = hash(password, salt);
-    pool.query('SELECT * FROM "user" username = $1' , [username], function(err,result){
+    pool.query('SELECT * FROM "user" where username = $1' , [username], function(err,result){
        if(err){
            res.status(500).send(err.toString());
        } else{
