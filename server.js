b52e3700a3d0373046e690f3fe2bf2b7adc0fd3d
parmeet1402 <33989504+parmeet1402@users.noreply.github.com>
2018-03-07 04:56:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -161,7 +161,7 @@ app.get('/check-login', function(req,res){
 app.get('/logout', function(req,res){
     delete req.session.auth;
     res.send('Logged out');
-})
+});
 
 var pool=new Pool(config);
 app.get('/test-db',function(req,res){
