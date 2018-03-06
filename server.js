80332ac76cc1f6452b0041e8901dd78aac94f157
parmeet1402 <33989504+parmeet1402@users.noreply.github.com>
2018-03-07 02:21:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.get('/', function (req, res) {
 function hash(input, salt){
     //how do we create the hash
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return hashed;
+    return hashed.toString('hex');
 }
 
 
