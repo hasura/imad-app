3adebfb34ce55e47c8f375102f831b6dd96fc853
panditdeepakgautam <37553476+panditdeepakgautam@users.noreply.github.com>
2018-03-23 08:48:40 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function(req, res){
+res.send('article one requested and will be served her');
+});
+
+app.get('/article-two', function(req, res){
+res.send('article two requested and will be served her');
+});
+
+app.get('/article-three', function(req, res){
+res.send('article three requested and will be served her');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
