f3ddfb0d77cffecdb074eaa84b14ad5bea10aa4c
panditdeepakgautam <37553476+panditdeepakgautam@users.noreply.github.com>
2018-03-26 20:24:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,27 @@
 console.log('Loaded!');
- var button = document.getElementById("Lincoln");
- button.onclick = function()
- {
-     alert("hi");
- };
+ window.onload = initAll;
+function initAll()
+{ 
+    document.getElementById("Lincoln").onclick = saySomething;
+ 
+    document.getElementById("kennedy").onclick = saySomething;
+  
+    document.getElementById("NIxon").onclick = saySomething;
+}
+
+function saySomething()
+{
+    switch(this.id)
+    {
+        case "Lincoln":
+            alert("four score and seven years ago..");
+            break;
+            case "kennedy":
+                alert("ask not what your country do four you...");
+                break;
+                case "Nixon":
+                    alert("i am not a crook");
+                    break;
+                default:
+    }
+}
