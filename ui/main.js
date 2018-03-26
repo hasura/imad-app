136982e64c464a7b1aebe6dba6e2d781164543b5
panditdeepakgautam <37553476+panditdeepakgautam@users.noreply.github.com>
2018-03-27 01:40:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,11 +2,11 @@ console.log('Loaded!');
  window.onload = initAll;
 function initAll()
 { 
-    document.getElementById("Lincoln").onclick = saySomething;
+    document.getElementById("Lincoln").onclick = saySomething();
  
-    document.getElementById("kennedy").onclick = saySomething;
+    document.getElementById("kennedy").onclick = saySomething();
   
-    document.getElementById("NIxon").onclick = saySomething;
+    document.getElementById("NIxon").onclick = saySomething();
 }
 
 function saySomething()
