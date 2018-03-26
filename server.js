cb20369dac0b91f4b29775c0894e2028915f742c
panditdeepakgautam <37553476+panditdeepakgautam@users.noreply.github.com>
2018-03-26 14:44:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function(req, res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-two', function(req, res){
