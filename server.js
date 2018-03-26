0db3cfa228e5003e0de04f304e01cb9e5faeae37
panditdeepakgautam <37553476+panditdeepakgautam@users.noreply.github.com>
2018-03-26 15:14:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/page1', function(req, res){
-  res.sendFile(path.join(__dirname, 'ui', 'page1.html'));
+app.get('/deepak', function(req, res){
+  res.sendFile(path.join(__dirname, 'ui', 'deepak.html'));
 });
 
 app.get('/article-two', function(req, res){
