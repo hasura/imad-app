1b014ce92dc4979e2adf5524fdc3b35f45956942
panditdeepakgautam <37553476+panditdeepakgautam@users.noreply.github.com>
2018-03-29 12:40:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -37,3 +37,29 @@ var port = 80;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
+window.onload = initAll;
+function initAll()
+{ 
+    document.getElementById("Lincoln").onclick = saySomething ;
+ 
+    document.getElementById("kennedy").onclick = saySomething ;
+  
+    document.getElementById("NIxon").onclick = saySomething ;
+}
+
+function saySomething()
+{
+    switch(this.id)
+    {
+        case "Lincoln":
+            alert("four score and seven years ago..");
+            break;
+            case "kennedy":
+                alert("ask not what your country do four you...");
+                break;
+                case "Nixon":
+                    alert("i am not a crook");
+                    break;
+                default:
+    }
+}
