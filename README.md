78ccb18ccc1defaa8e5f4b8f0f497f2693c87225
VISHNU-LMNTRIX <36585725+VISHNU-LMNTRIX@users.noreply.github.com>
2018-03-31 22:48:31 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# imad-app
+
+IMAD course application.
---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
+
+var app = express();
+app.use(morgan('combined'));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+
+// Do not change port, otherwise your app won't run on IMAD servers
+// Use 8080 only for local development if you already have apache running on 80
+
+var port = 80;
+app.listen(port, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
