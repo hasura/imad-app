a8263c733cb3dce791809b3f67f768dc3cc0a83e
panditdeepakgautam <37553476+panditdeepakgautam@users.noreply.github.com>
2018-04-08 10:32:43 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,13 @@
 console.log('loaded!');
-window.onload = initAll;
-
-function initAll() {
-    document.getElementById("lincoln").onclick = saySomething;
+window.onload = choosePic;
+function choosePic() {
+   function myPic() {
+        var myPic= new Array("https://3.imimg.com/data3/OL/CG/MY-1054197/832_834_pink-black-2-500x500.jpg","http://cdn.fcglcdn.com/brainbees/images/products/438x531/620138a.jpg","https://image.smythstoys.com/original/desktop/151922_7.jpg");
+    
+var randomNum = Math.floor((Math.random() * myPix.length));
+document.getElementById("myPicture").src = myPix[randomNum];
+    }
 }
 
-function saySomething()
-{
-     
-}
+ 
+ 
