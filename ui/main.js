e795645394ba70ce5799194fbd9eaf10b69715a2
panditdeepakgautam <37553476+panditdeepakgautam@users.noreply.github.com>
2018-04-08 17:56:11 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,4 +10,30 @@ document.getElementById("myPicture").src = myPix[randomNum];
     
   
 
- 
+ window.onload = choosePic;
+var youPix = new Array("http://freepngimg.com/download/technology/35661-2-robot-transparent.png","https://img00.deviantart.net/baa5/i/2008/185/c/f/xcc_900_terminator_001_by_selficide_stock.png","http://freepngimg.com/download/technology/35747-4-robot-transparent-background.png");
+var thisPic = 0;
+
+function choosePic() {
+    document.getElementById("prevLink").onclick = processPrevious;
+    document.getElementById("nextLink").onclick = processNext;
+}
+
+function processPrevious() {
+    if(thisPic === 0) {
+        thisPic = youPix.length;
+    }
+    thisPic--;
+    document.getElementById("youPicture").src = youPix[thisPic];
+    return false;
+} 
+
+function processNext() {
+   thisPic++ ;
+      if (thisPic === youPix.length)
+         {
+             thisPic = 0;
+         }
+    document.getElementById("youPicture").src = youPix[thisPic];
+    return false;
+} 
