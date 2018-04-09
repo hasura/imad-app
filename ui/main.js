0409d0c24ec14086c00a318d092079fa0b119e37
panditdeepakgautam <37553476+panditdeepakgautam@users.noreply.github.com>
2018-04-09 06:08:43 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,15 @@
 console.log('loaded!');
- 
+window.onload = choosePic; 
 function choosePic() {
     
         var myPix= new Array("https://3.imimg.com/data3/OL/CG/MY-1054197/832_834_pink-black-2-500x500.jpg","http://cdn.fcglcdn.com/brainbees/images/products/438x531/620138a.jpg","https://image.smythstoys.com/original/desktop/151922_7.jpg");
-    
-var randomNum = Math.floor((Math.random() * myPix.length));
+    var randomNum = Math.floor((Math.random() * myPix.length));
 document.getElementById("myPicture").src = myPix[randomNum];
+
+ var youPix= new Array("https://3.imimg.com/data3/OL/CG/MY-1054197/832_834_pink-black-2-500x500.jpg","http://cdn.fcglcdn.com/brainbees/images/products/438x531/620138a.jpg","https://image.smythstoys.com/original/desktop/151922_7.jpg");
+
+var randomNu = Math.floor((Math.random() * youPix.length));
+document.getElementById("youPicture").src = youPix[randomNu];
     }
     
  
