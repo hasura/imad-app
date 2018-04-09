74d0229fe67834e84bf4e7f3ac3c826b1b53cebd
panditdeepakgautam <37553476+panditdeepakgautam@users.noreply.github.com>
2018-04-08 17:26:40 -0700

    [imad-console] Updates ui/next.js

--- ui/next.js
+++ ui/next.js
@@ -1,4 +1,15 @@
 console.log('loaded!');
+window.onload = choosePic;
+function choosePic() {
+    
+        var myPix= new Array("https://3.imimg.com/data3/OL/CG/MY-1054197/832_834_pink-black-2-500x500.jpg","http://cdn.fcglcdn.com/brainbees/images/products/438x531/620138a.jpg","https://image.smythstoys.com/original/desktop/151922_7.jpg");
+    
+var randomNum = Math.floor((Math.random() * myPix.length));
+document.getElementById("myPicture").src = myPix[randomNum];
+    }
+    
+  
+
 window.onload = initLinks;
 var youPix = new Array("http://freepngimg.com/download/technology/35661-2-robot-transparent.png","https://img00.deviantart.net/baa5/i/2008/185/c/f/xcc_900_terminator_001_by_selficide_stock.png","http://freepngimg.com/download/technology/35747-4-robot-transparent-background.png");
 var thisPic = 0;
