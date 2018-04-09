6e7fc2f10f13e4432b88a2bf38acdf1c1512b5de
panditdeepakgautam <37553476+panditdeepakgautam@users.noreply.github.com>
2018-04-09 06:04:52 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,5 @@
 console.log('loaded!');
-window.onload = choosePic; 
-window.onload = chooseP;
+ 
 function choosePic() {
     
         var myPix= new Array("https://3.imimg.com/data3/OL/CG/MY-1054197/832_834_pink-black-2-500x500.jpg","http://cdn.fcglcdn.com/brainbees/images/products/438x531/620138a.jpg","https://image.smythstoys.com/original/desktop/151922_7.jpg");
