be52d8afb8538bcebd15f08105f47918123b1daa
panditdeepakgautam <37553476+panditdeepakgautam@users.noreply.github.com>
2018-04-09 12:39:27 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,6 +10,12 @@ document.getElementById("myPicture").src = myPix[randomNum];
 
 var randomNu = Math.floor((Math.random() * youPix.length));
 document.getElementById("youPicture").src = youPix[randomNu];
+
+
+ var thatPix= new Array("https://3.imimg.com/data3/OL/CG/MY-1054197/832_834_pink-black-2-500x500.jpg","http://cdn.fcglcdn.com/brainbees/images/products/438x531/620138a.jpg","https://image.smythstoys.com/original/desktop/151922_7.jpg");
+
+var randomN = Math.floor((Math.random() * thatPix.length));
+document.getElementById("thatPicture").src = thatPix[randomN];
  
     }
     
