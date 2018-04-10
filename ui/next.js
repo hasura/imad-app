48aabb752bbf06204b1c9ac99b9f416a0454a77e
panditdeepakgautam <37553476+panditdeepakgautam@users.noreply.github.com>
2018-04-09 23:29:07 -0700

    [imad-console] Updates ui/next.js

--- ui/next.js
+++ ui/next.js
@@ -1,2 +1,13 @@
-
+console.log('loaded!');
+ window.onload = initImages;
+function initImages() {
+for ( var i=0; i<document.images.length; i++) {
+     document.images[i].ondbclick = newWindow;
  
+    } }
+    
+ function newWindow() {
+     var imgName = "images/" + this.id + ".jpg";
+     var imgWindow = window.open(imgName,"imgWin", "width=320, height=240, scrollbars");
+     
+ }
