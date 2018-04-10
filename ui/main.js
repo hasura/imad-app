4bc02164dbbcf8bcae5bd8ad36a742f6dc448893
panditdeepakgautam <37553476+panditdeepakgautam@users.noreply.github.com>
2018-04-09 23:25:26 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,8 +16,18 @@ document.getElementById("youPicture").src = youPix[randomNu];
 
 var randomN = Math.floor((Math.random() * thatPix.length));
 document.getElementById("thatPicture").src = thatPix[randomN];
+}
+
+window.onload = initImages;
+function initImages() {
+for ( var i=0; i<document.images.length; i++) {
+     document.images[i].ondbclick = newWindow;
  
-    }
+    } }
     
- 
+ function newWindow() {
+     var imgName = "images/" + this.id + ".jpg";
+     var imgWindow = window.open(imgName,"imgWin", "width=320, height=240, scrollbars");
+     
+ }
  
