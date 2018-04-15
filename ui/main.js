8f0d99727273a5963de4cf34c5fcd30bc3873dc9
panditdeepakgautam <37553476+panditdeepakgautam@users.noreply.github.com>
2018-04-15 07:59:13 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,5 +18,16 @@ document.getElementById("thatPicture").src = thatPix[randomN];
  
 }
 
+function choosePic() {
+    for(var i=0; i<document.iamges.length; i++)
+    {
+        document.images[i].ondbclick = newWindow;
+    }
+}
 
+function newWindow() {
+    var imgName ="images/" +this.id + "" ;
+    var imgWindow = window.open(imgName, "imgWin", "width=320, height=240, scrollbars=no");
+    
+}
  
