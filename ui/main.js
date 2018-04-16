557f63e290860959b0f0c9101730dc98f4c42f09
panditdeepakgautam <37553476+panditdeepakgautam@users.noreply.github.com>
2018-04-16 14:09:55 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,3 +31,24 @@ function newWindow() {
     
 }
  
+ var myName = "Codecademy";
+
+var red = [0, 100, 63];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
+var letterColors = [red, orange, green, blue, purple];
+
+drawName(myName, letterColors);
+
+if(10 < 3)
+{
+    bubbleShape = 'square';
+}
+else
+{
+    bubbleShape = 'circle';
+}
+
+bounceBubbles();
