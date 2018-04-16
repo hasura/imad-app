9fb47303b5f82b4e3886a6324d4b939fbdb10707
panditdeepakgautam <37553476+panditdeepakgautam@users.noreply.github.com>
2018-04-16 14:11:04 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,37 +18,4 @@ document.getElementById("thatPicture").src = thatPix[randomN];
  
 }
 
-function choose() {
-    for(var i=0; i<document.iamges.length; i++)
-    {
-        document.images[i].ondbclick = newWindow;
-    }
-}
-
-function newWindow() {
-    var imgName ="images/" +this.id + "" ;
-    var imgWindow = window.open(imgName, "imgWin", "width=320, height=240, scrollbars=no");
-    
-}
  
- var myName = "Codecademy";
-
-var red = [0, 100, 63];
-var orange = [40, 100, 60];
-var green = [75, 100, 40];
-var blue = [196, 77, 55];
-var purple = [280, 50, 60];
-var letterColors = [red, orange, green, blue, purple];
-
-drawName(myName, letterColors);
-
-if(10 < 3)
-{
-    bubbleShape = 'square';
-}
-else
-{
-    bubbleShape = 'circle';
-}
-
-bounceBubbles();
