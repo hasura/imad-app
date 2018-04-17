fb302ae265f8009c2633de6ff1e014e0f92a4bcf
akashc777 <akashc777@gmail.com>
2018-04-17 22:00:35 +0530

    Added PORT = process.env.PORT || 5000

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+const PORT = process.env.PORT || 5000
 
 var app = express();
 app.use(morgan('combined'));
@@ -142,7 +143,7 @@ app.get('/ui/madi.png', function (req, res) {
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
-var port = 80;
-app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
+
+app.listen(PORT, function () {
+  console.log(`IMAD course app listening on port ${PORT}!`);
 });
