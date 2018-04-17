cbb87f2fbd0667c3ceddae6f60e6ac76b734ff7a
akashc777 <akashc777@gmail.com>
2018-04-17 22:43:56 +0530

    fixed port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-const PORT = PORT = process.env.PORT || 5000
+const PORT =  process.env.PORT || 5000
 
 var app = express();
 app.use(morgan('combined'));
