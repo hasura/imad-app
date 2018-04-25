ea31a08b3f634a7dfc8e69d8a299edf59e618125
Gayathrirnair <gayathrinairrajeev@gmail.com>
2018-04-25 21:45:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article-one',function(req,res){
+    res.send('article one is served ');
+});
+app.get('/article-two',function(req,res){
+    res.send('article two is served ');
+});
+app.get('/article-three',function(req,res){
+    res.send('article three is served ');
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
