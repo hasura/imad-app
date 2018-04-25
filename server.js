4d2ebe3da7ce72c542e3f9789c80c93ff8af2531
Gayathrirnair <gayathrinairrajeev@gmail.com>
2018-04-25 22:01:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(req,res){
-    res.send('article one is served ');
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two',function(req,res){
     res.send('article two is served ');
